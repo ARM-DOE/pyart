6c4f6d5418e2212a213f1b4783f285cc124a35ea
Jonathan Helmus <jjhelmus@gmail.com>
2013-02-07 14:12:53 -0600

    _Filled value define for fields from mdv files

--- io/radar.py
+++ io/radar.py
@@ -764,8 +764,9 @@ class Radar:
             data[where(data == 131072)] = -9999.0
             meta = self.get_mdv_meta(radarobj, field)  # fetch metadata
             fielddict = {
-                'data': ma.masked_equal(data, -9999.0).reshape(data.shape[0] *
-                data.shape[1], data.shape[2])}
+                'data': ma.masked_equal(data, -9999.0).reshape(
+                    data.shape[0] * data.shape[1], data.shape[2]),
+                '_FillValue': -9999.0}
             fielddict.update(meta)
             flat_dict.update({csapr_standard_names()[field]: fielddict})
         self.fields = flat_dict
