5c92f5c3e889188d56e6d39eda29b3f871a311cf
Jonathan Helmus <jjhelmus@gmail.com>
2013-03-13 15:45:50 -0500

    graph reorganization : cleaned common.py

--- pyart/graph/common.py
+++ pyart/graph/common.py
@@ -1,30 +1,44 @@
+"""
+pyart.graph.common
+==================
+
+Common graphing routines.
+
+.. autosummary::
+    :toctree: generated/
+
+    dms_to_d
+    radar_coords_to_cart
+    corner_to_point
+    ax_radius
+
+"""
 
 import numpy as np
 
 PI = 3.141592653589793
 
-
-def dms_to_d(dms):
-    return dms[0] + (dms[1] + dms[2] / 60.0) / 60.0
-
-
-def dt_to_dict(dt, **kwargs):
-    """
-    Returns a dictionary from a datetime object perfect for use in a
-    formatted string usage:
-        dict = (datetime.datetime object, pref = prefix
-                string for the dictionary keys)
-
-    """
-    pref = kwargs.get('pref', '')
-    return dict([(pref+key, getattr(dt, key)) for key in
-                ['year', 'month', 'day', 'hour', 'minute', 'second']])
+from ..io.common import dms_to_d, radar_coords_to_cart
 
 
 def corner_to_point(corner, point):
     """
-    Distance from a corner to a point in lat-lons given a spherical earth
-    usage: x_distance, y_distance=corner_to_point([lat1, lon1], [lat2, lon2])
+    Return the x, y distances in meters from a corner to a point.
+
+    Assumes a spherical earth model.
+
+    Parameters
+    ----------
+    corner : (float, float)
+        Latitude and longitude in degrees of the corner.
+    point : (float, float)
+        Latitude and longtide in degrees of the point.
+
+    Returns
+    -------
+    x, y : floats
+        Distances from the corner to the point in meters.
+
     """
     Re = 6371.0 * 1000.0
     Rc = ax_radius(point[0], units='degrees')
@@ -35,8 +49,21 @@ def corner_to_point(corner, point):
 
 
 def ax_radius(lat, units='radians'):
-    """Determine the radius of a circle of constant longitude at a certain
-    Latitude usage radius=(latitude, units='degrees' or 'radians')
+    """
+    Return the radius of a constant longitude circle at a given latitude.
+
+    Parameters
+    ----------
+    lat : float
+        Latitude to calculate constant longitude circle radius.
+    units : 'radians' or 'degrees'
+        Units of lat, either 'radians' or 'degrees'.
+
+    Returns
+    -------
+    R : float
+        Radius in meters of a constant longitude circle at latitude.
+
     """
     Re = 6371.0 * 1000.0
     if units == 'degrees':
@@ -45,22 +72,3 @@ def ax_radius(lat, units='radians'):
         const = 1.0
     R = Re * np.sin(PI / 2.0 - abs(lat * const))
     return R
-
-
-def radar_coords_to_cart(rng, az, ele, debug=False):
-    """
-    Asumes standard atmosphere, ie R=4Re/3
-    rng in km
-    """
-    Re = 6371.0 * 1000.0
-    p_r = 4.0 * Re / 3.0
-    rm = rng * 1000.0
-    z = (rm ** 2 + p_r ** 2 + 2.0 * rm * p_r *
-         np.sin(ele * PI / 180.0)) ** 0.5 - p_r
-    #arc length
-    s = p_r * np.arcsin(rm * np.cos(ele * PI / 180.) / (p_r + z))
-    if debug:
-        print "Z=", z, "s=", s
-    y = s * np.cos(az * PI / 180.0)
-    x = s * np.sin(az * PI / 180.0)
-    return x, y, z
