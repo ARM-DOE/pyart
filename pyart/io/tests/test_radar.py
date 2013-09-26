2e8748fa8401e07297631a1e494c1ba7b9521ac8
Jonathan Helmus <jjhelmus@gmail.com>
2013-09-26 14:28:41 -0500

    ENH: info method added to Radar object

--- pyart/io/tests/test_radar.py
+++ pyart/io/tests/test_radar.py
@@ -1,5 +1,8 @@
 """ Unit Tests for Py-ART's io/radar.py module. """
 
+import sys
+from io import BytesIO
+
 import numpy as np
 from numpy.testing import assert_raises
 import pyart
@@ -43,3 +46,34 @@ def test_add_field_like():
 def test_add_field_like_errors():
     radar = pyart.testing.make_target_radar()
     assert_raises(ValueError, radar.add_field_like, 'foo', 'bar', [])
+
+
+def test_info_levels():
+    for level in ['standard', 's', 'compact', 'c', 'full', 'f']:
+        yield check_info, level
+
+
+def test_info_nonstandard():
+    # build a non-standard radar object for testing all paths in info
+    radar = pyart.testing.make_target_radar()
+    radar.fields['reflectivity_horizontal']['data'] = [1, 2, 3, 4]
+    radar.instrument_parameters = {'foobar': {'data': [1, 2], 'bar': 'foo'}}
+    radar.radar_calibration = {'foobar': {'data': [1, 2], 'bar': 'foo'}}
+    check_info('standard', radar)
+
+
+def check_info(level, radar=None):
+    out = BytesIO()
+    get_info(level, out, radar)
+    # don't check the output, just that something was printed.
+    assert len(out.getvalue()) != 0
+
+
+def get_info(level='standard', out=sys.stdout, radar=None):
+    if radar is None:
+        radar = pyart.testing.make_target_radar()
+    radar.info(level, out)
+
+
+def test_info_errors():
+    assert_raises(ValueError, check_info, 'foo')
