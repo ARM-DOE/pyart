5cf3c312d1f71e27eff4e5e90cbb7b5458c04a61
Jonathan Helmus <jjhelmus@gmail.com>
2014-03-07 12:11:52 -0600

    MAINT: refactor of RadarMapDisplay object
    RadarMapDisplay parameters now more in line with a merging of RadarDisplay
    and GridMapDisplay.

--- pyart/graph/radarmapdisplay.py
+++ pyart/graph/radarmapdisplay.py
@@ -13,47 +13,107 @@ Class for creating plots on a geographic map using a Radar objects.
 """
 
 import numpy as np
+from mpl_toolkits.basemap import Basemap
+from mpl_toolkits.basemap import pyproj
 
 from .radardisplay import RadarDisplay
 
-#Here we have an added method that inherits the radar
-#display and allows the plotting on a basemap object
 
 class RadarMapDisplay(RadarDisplay):
-    try:
-        from mpl_toolkits.basemap import Basemap, pyproj
-    except ImportError:
-        print("Requires mpl_toolkits.basemap please install")
-        raise
-    def plot_ppi_map(self, field, tilt, mask_tuple=None, vmin=None, vmax=None,
-                 cmap='jet', mask_outside=True, title=None, title_flag=True,
-                 axislabels=(None, None), axislabels_flag=True,
-                 colorbar_flag=True, colorbar_label=None, ax=None, fig=None,
-                 lat_lines =  np.arange(30,46,1), lon_lines =  np.arange(-110,-75,1),
-                 box = None, resolution = 'h', shapefile = None):
+    """
+    A display object for creating plots on a geographic map from data in a
+    Radar object.
+
+    This class is still an in progress addition to Py-ART.  Some
+    functionality may not work correctly. Please report any problems to
+    the Py-ART GitHub Issue Tracker.
+
+    Parameters
+    ----------
+    radar : Radar
+        Radar object to use for creating plots.
+    shift : (float, float)
+        Shifts in km to offset the calculated x and y locations.
+
+    Attributes
+    ----------
+    plots : list
+        List of plots created.
+    plot_vars : list
+        List of fields plotted, order matches plot list.
+    cbs : list
+        List of colorbars created.
+    radar_name : str
+        Name of radar.
+    origin : str
+        'Origin' or 'Radar'.
+    shift : (float, float)
+        Shift in meters.
+    x, y, z : array
+        Cartesian location of a sweep in meters.
+    loc : (float, float)
+        Latitude and Longitude of radar in degrees.
+    time_begin : datetime
+        Beginning time of first radar scan.
+    starts : array
+        Starting ray index for each sweep.
+    ends : array
+        Ending ray index for each sweep.
+    fields : dict
+        Radar fields.
+    scan_type : str
+        Scan type.
+    ranges : array
+        Gate ranges in meters.
+    azimuths : array
+        Azimuth angle in degrees.
+    elevations : array
+        Elevations in degrees.
+    fixed_angle : array
+        Scan angle in degrees.
+    proj : Proj
+        Object for performing cartographic transformations specific to the
+        geographic map plotted.
+    basemap : Basemap
+        Last plotted basemap, None when no basemap has been plotted.
+
+
+    """
+
+    def __init__(self, radar, shift=(0.0, 0.0)):
+        """ Initialize the object. """
+
+        # initalize the base class
+        RadarDisplay.__init__(self, radar, shift=shift)
+
+        # additional Map attributes.
+        self.basemap = None
+        self.proj = pyproj.Proj(proj='lcc', datum='NAD83',
+                                lat_0=self.loc[0], lon_0=self.loc[1],
+                                x_0=0.0, y_0=0.0)
+        return
+
+    def plot_ppi_map(self, field, sweep=0, mask_tuple=None,
+                     vmin=None, vmax=None, cmap='jet', mask_outside=True,
+                     title=None, title_flag=True,
+                     colorbar_flag=True, colorbar_label=None,
+                     ax=None, fig=None,
+                     lat_lines=None, lon_lines=None,
+                     auto_range=True,
+                     min_lon=-92, max_lon=-86, min_lat=40, max_lat=44,
+                     resolution='h', shapefile=None):
         """
-        Plot a PPI.
+        Plot a PPI volume sweep onto a geographic map.
 
         Parameters
         ----------
         field : str
             Field to plot.
-        tilt : int,
-            Tilt number to plot.
+        sweep : int, optional
+            Sweep number to plot.
 
         Other Parameters
         ----------------
-        lat_lines : (array)
-            Array contianing the latitude lines to plot
-        lon_lines : (array)
-            Array contianing the longitude lines to plot
-        box : (list)
-            [min_lon, max_lon, min_lat, max_lat] The bounds of plot, leave to None
-            for the method to auto determine.
-        shapefile : (string)
-            filename for a esri shapefile as background (untested)
-        resolution : (string)
-            resolution parameter passed to basemap
         mask_tuple : (str, float)
             Tuple containing the field name and value below which to mask
             field prior to plotting, for example to mask all data where
@@ -73,12 +133,6 @@ class RadarMapDisplay(RadarDisplay):
             is False.
         title_flag : bool
             True to add a title to the plot, False does not add a title.
-        axislabels : (str, str)
-            2-tuple of x-axis, y-axis labels.  None for either label will use
-            the default axis label.  Parameter is ignored if axislabels_flag is
-            False.
-        axislabel_flag : bool
-            True to add label the axes, False does not label the axes.
         colorbar_flag : bool
             True to add a colorbar with label to the axis.  False leaves off
             the colorbar.
@@ -89,58 +143,69 @@ class RadarMapDisplay(RadarDisplay):
             Axis to plot on. None will use the current axis.
         fig : Figure
             Figure to add the colorbar to. None will use the current figure.
+        lat_lines, lon_lines : array or None
+            Locations at which to draw latitude and longitude lines.
+            None will use default values which are resonable for maps of
+            North America.
+        auto_range : bool
+            True to determine map ranges from the extend of the radar data.
+            False will use the min_lon, max_lon, min_lat, and max_lat
+            parameters for the map range.
+        min_lat, max_lat, min_lon, max_lon : float
+            Latitude and longitude ranges for the map projection region in
+            degrees.  These parameter are not used if auto_range is True.
+        shapefile : str
+            Filename for a ESRI shapefile as background (untested).
+        resolution : 'c', 'l', 'i', 'h', or 'f'.
+            Resolution of boundary database to use. See Basemap documentation
+            for details.
 
         """
         # parse parameters
         ax, fig = self._parse_ax_fig(ax, fig)
         vmin, vmax = self._parse_vmin_vmax(field, vmin, vmax)
+        if lat_lines is None:
+            lat_lines = np.arange(30, 46, 1)
+        if lon_lines is None:
+            lon_lines = np.arange(-110, -75, 1)
 
         # get data for the plot
-        data = self._get_data(field, tilt, mask_tuple)
-        x, y = self._get_x_y(field, tilt)
+        data = self._get_data(field, sweep, mask_tuple)
+        x, y = self._get_x_y(field, sweep)
 
         # mask the data where outside the limits
         if mask_outside:
             data = np.ma.masked_outside(data, vmin, vmax)
 
-        #build the proj
-        pnyc = self.pyproj.Proj(proj='lcc',datum='NAD83',lat_0=self.loc[0],lon_0=self.loc[1],x_0=0.0,y_0=0.0)
-        lon, lat = pnyc(x*1000.0, y*1000.0, inverse=True)
+        # determine map region
+        lon, lat = self.proj(x * 1000.0, y * 1000.0, inverse=True)
 
-        #set up the map
-
-        if box == None :
-            #setup the map based on the ranges of lat and lons
+        if auto_range:
             max_lat = lat.max()
             max_lon = lon.max()
             min_lat = lat.min()
             min_lon = lon.min()
-        else:
-            min_lon = box[0]
-            max_lon = box[1]
-            min_lat = box[2]
-            max_lat = box[3]
 
-        m = self.Basemap(llcrnrlon=min_lon,llcrnrlat=min_lat,
-                    urcrnrlon=max_lon,urcrnrlat=max_lat,
-                    projection='mill',area_thresh=1000,
-                    resolution = resolution)
+        # plot the basemap
+        basemap = Basemap(llcrnrlon=min_lon, llcrnrlat=min_lat,
+                          urcrnrlon=max_lon, urcrnrlat=max_lat,
+                          projection='mill', area_thresh=1000,
+                          resolution=resolution, ax=ax)
+        basemap.drawcoastlines(linewidth=1.25)
+        basemap.drawstates()
+        basemap.drawparallels(lat_lines, labels=[True, False, False, False])
+        basemap.drawmeridians(lon_lines, labels=[False, False, False, True])
+        self.basemap = basemap
 
-        xd,yd = m(lon, lat)
-        print min_lat, max_lat
+        # plot the data and optionally the shape file
+        xd, yd = basemap(lon, lat)
+        pm = basemap.pcolormesh(xd, yd, data, vmin=vmin, vmax=vmax, cmap=cmap)
 
-        # plot the data
-
-        pm = m.pcolormesh(xd, yd, data, vmin=vmin, vmax=vmax, cmap=cmap)
-        m.drawcoastlines(linewidth=1.25)
-        m.drawstates()
-        m.drawparallels(lat_lines,labels=[1,0,0,0])
-        m.drawmeridians(lon_lines,labels=[0,0,0,1])
-        if shapefile != None :
-            shp_info = m.readshapefile(shapefile,'whatever',drawbounds=True)
+        if shapefile is not None:
+            basemap.readshapefile(shapefile, 'shapefile', ax=ax)
 
         if title_flag:
-            self._set_title(field, tilt, title, ax)
+            self._set_title(field, sweep, title, ax)
 
         # add plot and field to lists
         self.plots.append(pm)
@@ -149,6 +214,4 @@ class RadarMapDisplay(RadarDisplay):
         if colorbar_flag:
             self.plot_colorbar(mappable=pm, label=colorbar_label,
                                field=field, fig=fig)
-
-
-
+        return
