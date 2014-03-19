9ae101c27f311e2238ee00ecb76d2121453fdcc8
Kirk North <kirk.w.north@gmail.com>
2014-03-19 19:39:37 -0400

    ENH: Grid object
    Adding the ability to exclude fields during the creation
    of a Grid object, as well as the ability to add a new field
    with the 'add_field' attribute similar to the Radar object.
    Furthermore, I am adding some extra axes and dimensions to check
    for when creating a Grid object. These axes and dimensions are
    compatible with older versions of MMCG files.

--- pyart/io/grid.py
+++ pyart/io/grid.py
@@ -28,55 +28,62 @@ import netCDF4
 from .cfradial import _ncvar_to_dict, _create_ncvar
 
 
-def read_grid(filename):
+def read_grid(filename, exclude_fields=[]):
     """
     Read a netCDF grid file
 
     Parameters
     ----------
     filename : str
-        Filename of netCDF grid file to read
+        Filename of NetCDF grid file to read.
+        
+    Optional parameters
+    -------------------
+    exclude_fields : list
+        A list of field names to exclude from Grid.
 
     Returns
     -------
     grid : Grid
-        Grid object containing Grid data.
+        Grid object containing gridded data.
 
     """
-    ncobj = netCDF4.Dataset(filename, 'r')
-    ncvars = ncobj.variables
+    
+    ncobj = netCDF4.Dataset(filename, mode='r')
 
     # metadata
     metadata = dict([(k, getattr(ncobj, k)) for k in ncobj.ncattrs()])
 
     # axes
-    axes_keys = ['time', 'time_start', 'time_end',
-                 'z_disp', 'y_disp', 'x_disp',
-                 'alt', 'lat', 'lon']
-    axes = dict((k, _ncvar_to_dict(ncvars[k])) for k in axes_keys)
-
-    # read in the fields
+    axes_keys = ['time', 'time_start', 'time_end', 'base_time',
+                 'time_offset', 'z_disp', 'y_disp', 'x_disp',
+                 'alt', 'lat', 'lon', 'z', 'y', 'x', 'lev']
+    axes = dict((k, _ncvar_to_dict(ncobj.variables[k])) for k in axes_keys if
+                k in ncobj.variables)
+    
     # determine the correct shape of the fields
     # ARM standard required a time dimension, so the shape of the fields
     # in the file are (1, nz, ny, nx) but the field data should be shaped
     # (nz, ny, nx) in the Grid object
-    ncdims = ncobj.dimensions
-    field_shape = tuple([len(ncdims[i]) for i in ['nz', 'ny', 'nx']])
-    field_shape_with_time = (1, ) + field_shape  # 1, nz, ny, nx on disk
+    dim_keys = ['nz', 'ny', 'nx', 'z', 'y', 'x']
+    field_shape = tuple([len(ncobj.dimensions[i]) for i in dim_keys if
+                         i in ncobj.dimensions])
+    field_shape_with_time = (1, ) + field_shape
 
     # check all non-axes variables, those with the correct shape
     # are added to the field dictionary, if a wrong sized field is
     # detected a warning is raised
-    field_keys = [k for k in ncvars.keys() if k not in axes_keys]
+    field_keys = [k for k in ncobj.variables if k not in axes_keys and
+                  k not in exclude_fields]
     fields = {}
     for field in field_keys:
-        field_dic = _ncvar_to_dict(ncvars[field])
+        field_dic = _ncvar_to_dict(ncobj.variables[field])
         if field_dic['data'].shape == field_shape_with_time:
             field_dic['data'].shape = field_shape
             fields[field] = field_dic
         else:
             bad_shape = field_dic['data'].shape
-            warn('Field %s skipped, incorrect shape %s', (field, bad_shape))
+            warn('Field %s skipped due to incorrect shape' %field)
 
     return Grid(fields, axes, metadata)
 
@@ -97,7 +104,8 @@ def write_grid(filename, grid, format='NETCDF4'):
         details.
 
     """
-    ncobj = netCDF4.Dataset(filename, 'w', format=format)
+    
+    ncobj = netCDF4.Dataset(filename, mode='w', format=format)
 
     # create the time dimension
     ncobj.createDimension('time', None)
@@ -110,9 +118,9 @@ def write_grid(filename, grid, format='NETCDF4'):
     ncobj.createDimension('nx', nx)
 
     # axes variables
-    if 'base_time' in grid.axes.keys():
+    if 'base_time' in grid.axes:
         _create_ncvar(grid.axes['base_time'], ncobj, 'base_time', ())
-    if 'time_offset' in grid.axes.keys():
+    if 'time_offset' in grid.axes:
         _create_ncvar(grid.axes['time_offset'], ncobj, 'time_offset',
                       ('time',))
     _create_ncvar(grid.axes['time'], ncobj, 'time', ('time', ))
@@ -137,6 +145,7 @@ def write_grid(filename, grid, format='NETCDF4'):
         setattr(ncobj, k, v)
 
     ncobj.close()
+    
     return
 
 
@@ -160,6 +169,7 @@ def _read_grid_cf(filename):
     Grid object is most likely not writable.
 
     """
+    
     ncobj = netCDF4.Dataset(filename)
     ncvars = ncobj.variables
     fields = {}
@@ -194,6 +204,7 @@ def _read_grid_wrf(filename):
     Grid object is most likely not writable.
 
     """
+    
     ncobj = netCDF4.Dataset(filename)
     ncvars = ncobj.variables
     fields = {}
@@ -231,11 +242,14 @@ class Grid:
         Dictionary of metadata.
 
     """
+    
     def __init__(self, fields, axes, metadata):
-        """ initalize """
+        """ Initalize object. """
+        
         self.fields = fields
         self.metadata = metadata
         self.axes = axes
+        
         return
 
     def write(self, filename, format='NETCDF4'):
@@ -246,10 +260,37 @@ class Grid:
         ----------
         filename : str
             Filename to save to.
-        format : str, optional
-            NetCDF format, one of 'NETCDF4', 'NETCDF4_CLASSIC',
-            'NETCDF3_CLASSIC' or 'NETCDF3_64BIT'. See netCDF4 documentation
-            fordetails.
-
+            
+        Optional parameters
+        -------------------
+        format : str
+            Format can be one of 'NETCDF4', 'NETCDF4_CLASSIC',
+            'NETCDF3_CLASSIC' or 'NETCDF3_64BIT'
+            
         """
         write_grid(filename, self, format=format)
+        
+    def add_field(self, field_name, field_dic):
+        """
+        Add field to object.
+        
+        Parameters
+        ----------
+        field_name : str
+            Name of the field to add.
+        field_dic : dic
+            Dictionary containing the field data and metadata.
+        """
+        
+        nz, ny, nx = self.fields[self.fields.keys()[0]]['data'].shape
+        
+        if 'data' not in field_dic:
+            raise KeyError('Field dictionary must contain a "data" key')
+        if field_name in self.fields:
+            raise ValueError('A field with name %s already exists' %field_name)
+        if field_dic['data'].shape != (nz, ny, nx):
+            raise ValueError('Field has invalid shape')
+        
+        self.fields[field_name] = field_dic
+        
+        return
