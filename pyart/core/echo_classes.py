94d1db190189c62f43f0e8dfb8c4f2bb14482b57
Scott Collis <scollis.acrf@gmail.com>
2014-09-12 10:18:22 -0500

    ADD: Enumerated echo classifications

---
+++ pyart/core/echo_classes.py
@@ -0,0 +1,28 @@
+"""
+pyart.core.echo_classes
+===============
+
+A Module where enumerated echo classifications are held
+
+.. autosummary::
+    :toctree: generated/
+    :template: dev_template.rst
+
+    echo_classes
+
+"""
+
+CLASS_NOISE = 0 #returs for which there is no significant scattering
+CLASS_CLUTTER = 1 #Stationary persistant clutter.
+CLASS_NONMETEO = 2 # Non-stationary but non-weather related, eg birds/bragg etc
+CLASS_MULTITRIP = 3
+CLASS_LIQUID = 4
+CLASS_ICE = 5
+CLASS_MIXEDPHASE = 6
+
+
+#This string will be helpful when saving files etc.
+CLASS_METADATA = """Numerical classifications for dominant scatterer at gate:
+0: Returs for which there is no significant scattering
+1: Stationary persistant clutter.
+2: Non-stationary but non-weather related, eg birds/bragg etc"""
