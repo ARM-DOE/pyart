bb0a33e20013fe3a07ed77e0c92c4718d4f5d25b
Scott Collis <scollis.acrf@gmail.com>
2014-09-24 13:40:13 -0500

    ENH: Finished formatting classes

--- pyart/core/echo_classes.py
+++ pyart/core/echo_classes.py
@@ -19,10 +19,17 @@ CLASS_MULTITRIP = 3
 CLASS_LIQUID = 4
 CLASS_ICE = 5
 CLASS_MIXEDPHASE = 6
+CLASS_HAIL = 7
 
 
 #This string will be helpful when saving files etc.
 CLASS_METADATA = """Numerical classifications for dominant scatterer at gate:
 0: Returs for which there is no significant scattering
 1: Stationary persistant clutter.
-2: Non-stationary but non-weather related, eg birds/bragg etc"""
+2: Non-stationary but non-weather related, eg birds/bragg etc
+3: Multi-trip echoes beyond the MUR
+4: Liquid precipitation
+5: Solid (ice) precipitation
+6: Mixed ice and liquiq
+7: Dominant hail (ie hail contaminated)
+"""
