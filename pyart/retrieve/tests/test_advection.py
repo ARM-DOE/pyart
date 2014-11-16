1e77b4959ab8b851106b1e110b1cd298c10e0615
Scott Collis <scollis.acrf@gmail.com>
2014-11-16 11:06:51 -0600

    WIP:Added testing for grid shifting

--- pyart/retrieve/tests/test_advection.py
+++ pyart/retrieve/tests/test_advection.py
@@ -22,3 +22,21 @@ def test_grid_displacememt_pc():
             test_storm_2, 'reflectivity', 0) == (3,5)
 
 
+def test_grid_shift():
+    #create two guassian storms
+    tgrid0 = pyart.testing.make_normal_storm(10.0, [0.0,0.0])
+    tgrid1 = pyart.testing.make_normal_storm(10.0, [5.0,5.0])
+    #trim one, trim and shift the other
+    tgrid1_reduced = grid_shift(tgrid1, [0.0, 0.0],
+            trim_edges = 10, mask_range = [400, -9000])
+    tgrid0_shifted = grid_shift(tgrid0, [5.0, 5.0],
+            trim_edges = 10, mask_range = [400, -9000])
+    #take the difference
+    diff = tgrid0_shifted.fields['reflectivity']['data'][0,:,:]\
+            - tgrid1_reduced.fields['reflectivity']['data'][0,:,:]
+    #Assert that the difference is basically digital noise.
+    #This actual value on my MB Air is -1.65633898546e-21
+    assert diff.mean() < 1.0e-10
+
+
+
