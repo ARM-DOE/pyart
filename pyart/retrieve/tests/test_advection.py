e29aee9d4233d4ad4c2b84dc9cacedffbab23413
Scott Collis <scollis.acrf@gmail.com>
2014-11-16 12:06:07 -0600

    FIX: fixed bug in testing for advection

--- pyart/retrieve/tests/test_advection.py
+++ pyart/retrieve/tests/test_advection.py
@@ -8,18 +8,16 @@ import pyart
 def test_grid_displacement_pc():
     test_storm = pyart.testing.make_storm_grid()
     test_storm.fields['reflectivity']['data'] =\
-            test_storm.fields['reflectivity']['data'][:, 9:-11, 9:-1]
-    test_storm.fields['reflectivity']['data'] =\
             test_storm.fields['reflectivity']['data'][:, 5:-5, :]
-    test_storm = pyart.testing.make_storm_grid()
-    test_storm.fields['reflectivity']['data'] =\
-            test_storm.fields['reflectivity']['data'][:, 0:-10, :]
-    test_storm.fields['reflectivity']['data'][:,:,4:-3] =\
-            test_storm.fields['reflectivity']['data'][:, :, 1:-6]
-    test_storm.fields['reflectivity']['valid_min']=0.0
+    test_storm_2 = pyart.testing.make_storm_grid()
+    test_storm_2.fields['reflectivity']['data'] =\
+            test_storm_2.fields['reflectivity']['data'][:, 0:-10, :]
+    test_storm_2.fields['reflectivity']['data'][:,:,4:-3] =\
+            test_storm_2.fields['reflectivity']['data'][:, :, 1:-6]
+    test_storm_2.fields['reflectivity']['valid_min']=0.0
     test_storm.fields['reflectivity']['valid_min']=0.0
     assert pyart.retrieve.grid_displacement_pc(test_storm,
-            test_storm, 'reflectivity', 0) == (3,5)
+            test_storm_2, 'reflectivity', 0) == (3,5)
 
 
 def test_grid_shift():
