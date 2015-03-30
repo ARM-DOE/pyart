48aaec3cbff17e1077ae1f0b1dae8c479f22a187
Jonathan Helmus <jjhelmus@gmail.com>
2015-03-30 15:00:20 -0500

    MAINT: Cleanup of SNR from reflectivity PR
    * PEP8.
    * Fixed docstrings.
    * Use get_field_name to retrieve field metadata name.
    * Added new module to developer docs.

--- pyart/retrieve/simple_moment_calculations.py
+++ pyart/retrieve/simple_moment_calculations.py
@@ -1,72 +1,68 @@
 """
 pyart.retrieve.simple_moment_calculations
-=========================
+=========================================
 
-steiner_conv_strat
+Simple moment calculations.
 
 .. autosummary::
     :toctree: generated/
 
     calculate_snr_from_reflectivity
+
 """
 
 import numpy as np
 
-from ..config import get_metadata
+from ..config import get_metadata, get_field_name
 from ..io.common import radar_coords_to_cart
 
-def calculate_snr_from_reflectivity(radar,
-        reflectivity_key = None,
-        toa = None):
+
+def calculate_snr_from_reflectivity(
+        radar, refl_field=None, snr_field=None, toa=25000.):
     """
-    Back out the Signal to noise ratio, in dB, from the reflectivity field
+    Calculate the signal to noise ratio, in dB, from the reflectivity field.
+
+    Parameters
     ----------
     radar : Radar
-        Radar object
+        Radar object from which to retrieve reflectivity field.
+    refl_field : str, optional
+        Name of field in radar which contains the reflectivity.
+        None will use the default field name in the Py-ART configuration file.
+    snr_field : str, optional
+        Name to use for snr metadata.
+        None will use the default field name in the Py-ART configuration file.
+    toa : float, optional
+        Height above which to take noise floor measurements, in meters.
 
     Returns
     -------
     snr : field dictionary
-        Field dictionary containing the signal to noise ratio
-
-    Other Parameters
-    ----------------
-    reflectivity_key : string
-        In case reflectivity is not in reflectivity
-    toa : float
-        height above which to take noise floor measurements
-
-    References
-    ----------
+        Field dictionary containing the signal to noise ratio.
 
     """
-    if reflectivity_key == None:
-        reflectivity_key = 'reflectivity'
-
-    if toa == None:
-        toa = 25000.
-
+    if refl_field is None:
+        refl_field = get_field_name('reflectivity')
+    if snr_field is None:
+        snr_field = get_field_name('signal_to_noise_ratio')
 
     range_grid = np.meshgrid(radar.range['data'],
-                             np.ma.ones(radar.time['data'].shape))[0] +1.0
+                             np.ma.ones(radar.time['data'].shape))[0] + 1.0
 
     # remove range scale.. This is basically the radar constant scaled dBm
-    pseudo_power = radar.fields[reflectivity_key]['data']\
-            -20.0*np.log10(range_grid/1000.0)
+    pseudo_power = (radar.fields[refl_field]['data'] -
+                    20.0*np.log10(range_grid / 1000.0))
 
-    #Noise floor estimate
-    #25km.. should be no scatterers, not even planes, this high
-    #we could get undone by AP though.. also sun
-
-    rg, azg = np.meshgrid(radar.range['data'], radar.azimuth['data'] )
+    # Noise floor estimate
+    # 25km.. should be no scatterers, not even planes, this high
+    # we could get undone by AP though.. also sun
+    rg, azg = np.meshgrid(radar.range['data'], radar.azimuth['data'])
     rg, eleg = np.meshgrid(radar.range['data'], radar.elevation['data'])
-    x,y,z = radar_coords_to_cart(rg /1000.0, azg, eleg) #FLAG: need to fix
+    x, y, z = radar_coords_to_cart(rg / 1000.0, azg, eleg)  # XXX: need to fix
 
     points_above = np.where(z > toa)
     noise_floor_estimate = pseudo_power[points_above].mean()
-    snr_dict = get_metadata(radar.fields.keys()[0])
+
+    snr_dict = get_metadata(snr_field)
     snr_dict['data'] = pseudo_power - noise_floor_estimate
-    snr_dict['units'] = 'dB'
-    snr_dict['standard_name'] = 'SNR'
-    snr_dict['long_name'] = 'Signal to Noise Ratio'
     return snr_dict
