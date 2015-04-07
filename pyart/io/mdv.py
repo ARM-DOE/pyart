4eb76f58223babc97188baabc76531fc8032c70b
Anderson Gama <agama@simepar.br>
2015-04-07 14:44:35 -0300

    while reading mdv's, mask both bad_value and missing_data

--- pyart/io/mdv.py
+++ pyart/io/mdv.py
@@ -566,7 +566,7 @@ class MdvFile:
             # read the decompressed data, reshape and mask
             sw_data = np.fromstring(decompr_data, np_form).astype('float32')
             sw_data.shape = (ny, nx)
-            mask = sw_data == field_header['bad_data_value']
+            mask =  (sw_data == field_header['bad_data_value']) | (sw_data == field_header['missing_data_value'])
             np.putmask(sw_data, mask, [np.NaN])
             
             # scale and offset the data, store in field_data
