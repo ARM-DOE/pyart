7ac13221e7d464b2e37ce2d3f7f07c14d37c4161
Jonathan Helmus <jjhelmus@gmail.com>
2015-06-03 17:39:54 -0500

    PY3: Added Python 3 trove classifiers to setup.py

--- setup.py
+++ setup.py
@@ -37,6 +37,9 @@ Programming Language :: Python
 Programming Language :: Python :: 2
 Programming Language :: Python :: 2.6
 Programming Language :: Python :: 2.7
+Programming Language :: Python :: 3
+Programming Language :: Python :: 3.3
+Programming Language :: Python :: 3.4
 Programming Language :: C
 Programming Language :: Cython
 Programming Language :: Fortran
