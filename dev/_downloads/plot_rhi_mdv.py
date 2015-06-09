1ed90d1d27b0b3afe77f1e65bd7661b0bcf4c55e
Jonathan Helmus <jjhelmus@gmail.com>
2015-06-09 10:57:17 -0500

    Update documentation to release 1.4.0

--- dev/_downloads/plot_rhi_mdv.py
+++ dev/_downloads/plot_rhi_mdv.py
@@ -3,8 +3,7 @@
 Create a RHI plot from a MDV file
 =================================
 
-An example which creates a RHI plot of a MDV file using both a MdvDisplay
-and a RadarDisplay object.
+An example which creates a RHI plot of a MDV file using a RadarDisplay object.
 
 """
 print __doc__
@@ -17,18 +16,10 @@ import pyart
 
 filename = '110041.mdv'
 
-# create the plot using RadarDisplay (recommended)
+# create the plot using RadarDisplay
 radar = pyart.io.read_mdv(filename)
 display = pyart.graph.RadarDisplay(radar)
 fig = plt.figure(figsize=[5, 5])
 ax = fig.add_subplot(111)
 display.plot('reflectivity', 0, vmin=-16, vmax=64.0)
-plt.show()
-
-# create the plot using MdvDisplay
-mdvfile = pyart.io.mdv.MdvFile(filename)
-display = pyart.graph.MdvDisplay(mdvfile)
-fig = plt.figure(figsize=[5, 5])
-ax = fig.add_subplot(111)
-display.plot_rhi('DBZ_F', 0, vmin=-16, vmax=64.0)
 plt.show()
--- dev/_sources/dev_reference/io.txt
+++ dev/_sources/dev_reference/io.txt
@@ -9,11 +9,16 @@ Input/Output routines.
 .. automodule:: pyart.io.chl
 .. automodule:: pyart.io.common
 .. automodule:: pyart.io.grid_io
-.. automodule:: pyart.io.mdv
+.. automodule:: pyart.io.lazydict
+.. automodule:: pyart.io.mdv_common
+.. automodule:: pyart.io.mdv_radar
+.. automodule:: pyart.io.mdv_grid
+.. automodule:: pyart.io.nexradl3_read
 .. automodule:: pyart.io.nexrad_archive
 .. automodule:: pyart.io.nexrad_cdm
 .. automodule:: pyart.io.nexrad_common
 .. automodule:: pyart.io.nexrad_level2
+.. automodule:: pyart.io.nexrad_level3
 .. automodule:: pyart.io.rsl
 .. automodule:: pyart.io.sigmet
 .. automodule:: pyart.io._rsl_interface
