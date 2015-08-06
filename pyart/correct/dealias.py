45406b17731d7d66968c9da53dcd0127327cc747
Jonathan Helmus <jjhelmus@gmail.com>
2015-08-06 09:05:27 -0500

    MAINT: Remove depreciate parameters from dealias_fourdd

--- pyart/correct/dealias.py
+++ pyart/correct/dealias.py
@@ -27,10 +27,10 @@ from ..util import datetime_utils
 def dealias_fourdd(radar, last_radar=None, sounding_heights=None,
                    sounding_wind_speeds=None, sounding_wind_direction=None,
                    gatefilter=False, prep=1, filt=1, rsl_badval=131072.0,
-                   keep_original=False, extra_prep=False, ncp_min=0.3,
-                   rhv_min=0.7, refl_field=None, vel_field=None,
-                   corr_vel_field=None, last_vel_field=None, ncp_field=None,
-                   rhv_field=None, debug=False, max_shear=0.05, sign=1,
+                   keep_original=False,
+                   refl_field=None, vel_field=None,
+                   corr_vel_field=None, last_vel_field=None,
+                   debug=False, max_shear=0.05, sign=1,
                    **kwargs):
     """
     Dealias Doppler velocities using the 4DD algorithm.
@@ -86,20 +86,6 @@ def dealias_fourdd(radar, last_radar=None, sounding_heights=None,
         True to keep original doppler velocity values when the dealiasing
         procedure fails, otherwises these gates will be masked.  NaN values
         are still masked.
-    extra_prep : bool
-        True to use extra volume preperation, which will use the normalized
-        coherent power and cross correlation ratio fields to further remove
-        bad gates. Set this to False (default) if those fields are not
-        available. This argument has become deprecated with the introduction of
-        the radar gate filter class.
-    ncp_min : float
-        Minimum normalized coherent power allowed for any gate. Only
-        applicable when extra_prep is True. This threshold has become
-        deprecated with the introduction of the radar gate filter class.
-    rhv_min : float
-        Minimum cross correlation ratio allowed for any gate. Only
-        applicable when extra_prep is True. This threshold has become
-        deprecated with the introduction of the radar gate filter class.
     refl_field : str
         Field in radar to use as the reflectivity during dealiasing.
         None will use the default field name from the Py-ART configuration
@@ -114,12 +100,6 @@ def dealias_fourdd(radar, last_radar=None, sounding_heights=None,
     last_vel_field : str
         Name to use for the dealiased Doppler velocity field metadata in
         last_radar.  None will use the corr_vel_field name.
-    ncp_field, rhv_field : str
-        Fields in radar to use as the normalized coherent power and cross
-        correlation ratio, respectively. None will use the default field name
-        from the Py-ART configuration file. Only applicable when extra_prep
-        is True. These fields are deprecated with the introduction of the
-        radar gate filter class.
     maxshear : float
         Maximum vertical shear which will be incorperated into the created
         volume from the sounding data.  Parameter not used when no
@@ -170,35 +150,9 @@ def dealias_fourdd(radar, last_radar=None, sounding_heights=None,
         corr_vel_field = get_field_name('corrected_velocity')
     if last_vel_field is None:
         last_vel_field = get_field_name('corrected_velocity')
-    if ncp_field is None:
-        ncp_field = get_field_name('normalized_coherent_power')
-    if rhv_field is None:
-        rhv_field = get_field_name('cross_correlation_ratio')
 
     # get fill value
     fill_value = get_fillvalue()
-
-    # check extra volume preparation inputs
-    # this functionality has become deprecated with the introduction of the
-    # radar gate filter class
-    if extra_prep:
-        if ncp_field not in radar.fields and rhv_field not in radar.fields:
-            raise KeyError(('Radar does not have necessary fields for'
-                            'extra volume preparation'))
-
-    # extra volume preparation
-    # this assumes the radar has a normalized coherent power field and a
-    # cross correlation ratio field
-    # this functionality has become deprecated with the introduction of the
-    # radar gate filter class
-    if prep and extra_prep:
-        not_coherent = np.logical_or(
-            radar.fields[ncp_field]['data'] < ncp_min,
-            radar.fields[rhv_field]['data'] < rhv_min)
-        fdata = np.copy(radar.fields[refl_field]['data']).astype(np.float32)
-        fdata[not_coherent] = fill_value
-    else:
-        fdata = None
 
     # parse radar gate filter
     gatefilter = _parse_gatefilter(gatefilter, radar, **kwargs)
