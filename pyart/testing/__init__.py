40c51f36aabbb9e0e07212c71d254acf3af9a5e4
Jonathan Helmus <jjhelmus@gmail.com>
2015-08-25 09:25:04 -0500

    ENH: InMemoryFile StringIO/ByteIO alias in pyart.testing

--- pyart/testing/__init__.py
+++ pyart/testing/__init__.py
@@ -35,4 +35,11 @@ from .sample_objects import make_target_grid, make_storm_grid
 from .sample_objects import make_empty_rhi_radar
 from .sample_objects import make_velocity_aliased_rhi_radar
 
+# Python 2/3 StringIO object, not documented as numpydoc does not like
+# docstring and has issues finding/including methods.
+try:
+    from StringIO import StringIO as InMemoryFile
+except ImportError:
+    from io import BytesIO as InMemoryFile
+
 __all__ = [s for s in dir() if not s.startswith('_')]
