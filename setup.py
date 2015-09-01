8266279c1edaff57c03852c081be1a7efef92f71
Jonathan Helmus <jjhelmus@gmail.com>
2015-09-01 13:08:10 -0500

    MAINT: start v1.6+dev

--- setup.py
+++ setup.py
@@ -62,9 +62,9 @@ LICENSE = 'BSD'
 CLASSIFIERS = filter(None, CLASSIFIERS.split('\n'))
 PLATFORMS = ["Linux", "Mac OS-X", "Unix"]
 MAJOR = 1
-MINOR = 5
+MINOR = 6
 MICRO = 0
-ISRELEASED = True
+ISRELEASED = False
 VERSION = '%d.%d.%d' % (MAJOR, MINOR, MICRO)
 SCRIPTS = glob.glob('scripts/*')
 
