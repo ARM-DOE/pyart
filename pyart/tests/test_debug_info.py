df2c5bfecf69fd8a075da01c3588abb458a5e3b1
Jonathan Helmus <jjhelmus@gmail.com>
2015-09-08 13:22:40 -0500

    TST: Additional unit tests for _debug_info module

--- pyart/tests/test_debug_info.py
+++ pyart/tests/test_debug_info.py
@@ -1,4 +1,7 @@
-""" Unit Tests for Py-ART's config.py module. """
+""" Unit Tests for Py-ART's _debug_info module. """
+
+import sys
+import warnings
 
 import pyart
 try:
@@ -13,3 +16,37 @@ def test_debug_info():
     buf = StringIO()
     pyart._debug_info(buf)
     assert len(buf.getvalue()) > 0
+
+
+def test_debug_stdout():
+    # lack of error is assumed to mean that call succeed
+    pyart._debug_info()
+
+
+# Class for Mocking ImportErrors from
+# http://stackoverflow.com/questions/2481511/mocking-importerror-in-python
+class DisableModules(object):
+
+    def __init__(self, modules):
+        self.modules = modules
+
+    def find_module(self, fullname, path=None):
+        if fullname in self.modules:
+            raise ImportError('Debug import failure for %s' % fullname)
+
+
+def test_debug_info_all_disabled():
+    modules = ['numpy', 'scipy', 'matplotlib', 'netCDF4', 'cylp', 'glpk',
+               'cvxopt', 'mpl_toolkits', 'nose', 'pyart', 'platform']
+    for module in modules:
+        if module in sys.modules:
+            del sys.modules[module]
+    fail_loader = DisableModules(modules)
+    sys.meta_path.append(fail_loader)
+    with warnings.catch_warnings():
+        warnings.simplefilter('ignore')
+        # test to see that something is written when _debug_info is called
+        # we don't care what is written, just that something is.
+        buf = StringIO()
+        pyart._debug_info(buf)
+        assert len(buf.getvalue()) > 0
