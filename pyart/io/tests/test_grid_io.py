507f2196dfc8c312355157bf2d1f2430c612bfe7
Jonathan Helmus <jjhelmus@gmail.com>
2015-12-03 16:00:36 -0600

    TST: Additional test coverage of grid_io module

--- pyart/io/tests/test_grid_io.py
+++ pyart/io/tests/test_grid_io.py
@@ -3,7 +3,7 @@
 from __future__ import print_function
 
 import netCDF4
-from numpy.testing import assert_almost_equal
+from numpy.testing import assert_almost_equal, assert_warns
 
 import pyart
 
@@ -13,6 +13,7 @@ def test_grid_write_read():
     # write/read roundtrip and comparing the two Grid objects
     grid1 = pyart.testing.make_target_grid()
     grid1.projection['comment'] = 'This is a comment'
+    grid1.metadata['comment'] = 'This is another comment'
 
     with pyart.testing.InTemporaryDirectory():
         tmpfile = 'tmp_grid.nc'
@@ -94,3 +95,58 @@ def test_grid_write_point_vars():
         assert 'point_longitude' in dset.variables
         assert 'point_altitude' in dset.variables
         dset.close()
+
+
+def test_grid_write_arm_time_vars():
+    grid1 = pyart.testing.make_target_grid()
+
+    with pyart.testing.InTemporaryDirectory():
+        tmpfile = 'tmp_grid.nc'
+        pyart.io.write_grid(tmpfile, grid1, arm_time_variables=True)
+        dset = netCDF4.Dataset(tmpfile, 'r')
+        assert 'base_time' in dset.variables
+        assert 'time_offset' in dset.variables
+        dset.close()
+
+
+def test_exclude_fields_argument():
+    grid1 = pyart.testing.make_target_grid()
+    with pyart.testing.InTemporaryDirectory():
+        tmpfile = 'tmp_grid.nc'
+        pyart.io.write_grid(tmpfile, grid1)
+        grid2 = pyart.io.read_grid(tmpfile, exclude_fields=['reflectivity'])
+        assert 'reflectivity' in grid1.fields
+        assert 'reflectivity' not in grid2.fields
+
+
+def test_radar_attrs_none():
+    grid1 = pyart.testing.make_target_grid()
+    grid1.radar_latitude = None
+    grid1.radar_longitude = None
+    grid1.radar_altitude = None
+    grid1.radar_time = None
+    grid1.radar_name = None
+    with pyart.testing.InTemporaryDirectory():
+        tmpfile = 'tmp_grid.nc'
+        pyart.io.write_grid(tmpfile, grid1)
+        grid2 = pyart.io.read_grid(tmpfile)
+        assert grid2.radar_latitude is None
+        assert grid2.radar_longitude is None
+        assert grid2.radar_altitude is None
+        assert grid2.radar_time is None
+        assert grid2.radar_name is None
+
+
+def test_bad_shaped_field():
+    grid1 = pyart.testing.make_target_grid()
+    with pyart.testing.InTemporaryDirectory():
+        tmpfile = 'tmp_grid.nc'
+        pyart.io.write_grid(tmpfile, grid1)
+
+        # add a scalar variable named bad_field
+        dset = netCDF4.Dataset(tmpfile, 'a')
+        dset.createVariable('bad_field', 'f4', ())
+        dset.close()
+
+        # warning should be raised about incorrect shape of bad_field var
+        assert_warns(UserWarning, pyart.io.read_grid, tmpfile)
