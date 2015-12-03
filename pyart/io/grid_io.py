11fe999934455849132288dbaab98cf55da8bb03
Jonathan Helmus <jjhelmus@gmail.com>
2015-12-03 15:32:51 -0600

    ENH: read/write_grid uses new file layout
    The read_grid and write_grid function create NetCDF files with formats
    based upon the layout of the Grid class.

--- pyart/io/grid_io.py
+++ pyart/io/grid_io.py
@@ -1,6 +1,6 @@
 """
-pyart.io.grid
-=============
+pyart.io.grid_io
+================
 
 Reading and writing Grid objects.
 
@@ -49,36 +49,52 @@ def read_grid(filename, exclude_fields=None, **kwargs):
     if exclude_fields is None:
         exclude_fields = []
 
-    ncobj = netCDF4.Dataset(filename, mode='r')
+    reserved_variables = [
+        'time', 'regular_x', 'regular_y', 'regular_z',
+        'origin_latitude', 'origin_longitude', 'origin_altitude',
+        'point_x', 'point_y', 'point_z', 'projection',
+        'point_latitude', 'point_longitude', 'point_altitude',
+        'radar_latitude', 'radar_longitude', 'radar_altitude',
+        'radar_name', 'radar_time', 'base_time', 'time_offset']
+
+    dset = netCDF4.Dataset(filename, mode='r')
 
     # metadata
-    metadata = dict([(k, getattr(ncobj, k)) for k in ncobj.ncattrs()])
+    metadata = dict([(k, getattr(dset, k)) for k in dset.ncattrs()])
 
-    # axes
-    axes_keys = ['time', 'time_start', 'time_end', 'base_time',
-                 'time_offset', 'z_disp', 'y_disp', 'x_disp',
-                 'alt', 'lat', 'lon', 'z', 'lev', 'y', 'x']
-    axes = dict((k, _ncvar_to_dict(ncobj.variables[k])) for k in axes_keys
-                if k in ncobj.variables)
+    # required reserved variables
+    time = _ncvar_to_dict(dset.variables['time'])
+    origin_latitude = _ncvar_to_dict(dset.variables['origin_latitude'])
+    origin_longitude = _ncvar_to_dict(dset.variables['origin_longitude'])
+    origin_altitude = _ncvar_to_dict(dset.variables['origin_altitude'])
+    regular_x = _ncvar_to_dict(dset.variables['regular_x'])
+    regular_y = _ncvar_to_dict(dset.variables['regular_y'])
+    regular_z = _ncvar_to_dict(dset.variables['regular_z'])
+
+    # projection
+    projection = _ncvar_to_dict(dset.variables['projection'])
+    projection.pop('data')
+    # map _include_lon_0_lat_0 key to bool type
+    if '_include_lon_0_lat_0' in projection:
+        v = projection['_include_lon_0_lat_0']
+        projection['_include_lon_0_lat_0'] = {'true': True, 'false': False}[v]
 
     # read in the fields
-    # determine the correct shape of the fields
-    # ARM standard requires the left-most dimension to be time, so the shape
-    # of the fields in the file is (1, nz, ny, nx) but the field data should
-    # be shaped (nz, ny, nx) in the Grid object
-    dim_keys = ['nz', 'ny', 'nx', 'z', 'y', 'x']
-    field_shape = tuple([len(ncobj.dimensions[k]) for k in dim_keys
-                         if k in ncobj.dimensions])
+    fields = {}
+
+    # fields in the file has a shape of (1, nz, ny, nx) with the leading 1
+    # indicating time but should shaped (nz, ny, nx) in the Grid object
+    field_shape = tuple([len(dset.dimensions[d]) for d in ['nz', 'ny', 'nx']])
     field_shape_with_time = (1, ) + field_shape
 
-    # check all non-axes variables, those with the correct shape
+    # check all non-reserved variables, those with the correct shape
     # are added to the field dictionary, if a wrong sized field is
     # detected a warning is raised
-    field_keys = [k for k in ncobj.variables if k not in axes_keys and
-                  k not in exclude_fields]
-    fields = {}
+    field_keys = [k for k in dset.variables if k not in reserved_variables]
     for field in field_keys:
-        field_dic = _ncvar_to_dict(ncobj.variables[field])
+        if field in exclude_fields:
+            continue
+        field_dic = _ncvar_to_dict(dset.variables[field])
         if field_dic['data'].shape == field_shape_with_time:
             field_dic['data'].shape = field_shape
             fields[field] = field_dic
@@ -86,11 +102,43 @@ def read_grid(filename, exclude_fields=None, **kwargs):
             bad_shape = field_dic['data'].shape
             warn('Field %s skipped due to incorrect shape' % (field))
 
-    ncobj.close()
+    # radar_ variables
+    if 'radar_latitude' in dset.variables:
+        radar_latitude = _ncvar_to_dict(dset.variables['radar_latitude'])
+    else:
+        radar_latitude = None
 
-    return Grid(axes['time'], fields, metadata,
-                axes['lat'], axes['lon'], axes['alt'],
-                axes['x_disp'], axes['y_disp'], axes['z_disp'])
+    if 'radar_longitude' in dset.variables:
+        radar_longitude = _ncvar_to_dict(dset.variables['radar_longitude'])
+    else:
+        radar_longitude = None
+
+    if 'radar_altitude' in dset.variables:
+        radar_altitude = _ncvar_to_dict(dset.variables['radar_altitude'])
+    else:
+        radar_altitude = None
+
+    if 'radar_name' in dset.variables:
+        radar_name = _ncvar_to_dict(dset.variables['radar_name'])
+    else:
+        radar_name = None
+
+    if 'radar_time' in dset.variables:
+        radar_time = _ncvar_to_dict(dset.variables['radar_time'])
+    else:
+        radar_time = None
+
+    dset.close()
+
+    grid = Grid(
+        time, fields, metadata,
+        origin_latitude, origin_longitude, origin_altitude,
+        regular_x, regular_y, regular_z,
+        radar_latitude=radar_latitude, radar_longitude=radar_longitude,
+        radar_altitude=radar_altitude, radar_name=radar_name,
+        radar_time=radar_time)
+    grid.projection = projection
+    return grid
 
 
 def write_grid(filename, grid, format='NETCDF4', arm_time_variables=False):
@@ -127,32 +175,52 @@ def write_grid(filename, grid, format='NETCDF4', arm_time_variables=False):
         time_offset. False will not write these variables.
 
     """
-    ncobj = netCDF4.Dataset(filename, mode='w', format=format)
+    dset = netCDF4.Dataset(filename, mode='w', format=format)
 
-    # create the time dimension
-    ncobj.createDimension('time', None)
+    # create dimensions
+    dset.createDimension('time', None)
+    dset.createDimension('nz', grid.nz)
+    dset.createDimension('ny', grid.ny)
+    dset.createDimension('nx', grid.nx)
+    if grid.nradar != 0:
+        dset.createDimension('nradar', grid.nradar)
+        if grid.radar_name is not None:
+            nradar_str_length = len(grid.radar_name['data'][0])
+            dset.createDimension('nradar_str_length', nradar_str_length)
 
-    # create additional dimensions
-    grid_shape = grid.fields[list(grid.fields.keys())[0]]['data'].shape
-    nz, ny, nx = grid_shape
-    ncobj.createDimension('nz', nz)
-    ncobj.createDimension('ny', ny)
-    ncobj.createDimension('nx', nx)
+    # required variables
+    _create_ncvar(grid.time, dset, 'time', ('time', ))
+    _create_ncvar(grid.regular_x, dset, 'regular_x', ('nx', ))
+    _create_ncvar(grid.regular_y, dset, 'regular_y', ('ny', ))
+    _create_ncvar(grid.regular_z, dset, 'regular_z', ('nz', ))
+    _create_ncvar(grid.origin_latitude, dset, 'origin_latitude', ('time', ))
+    _create_ncvar(grid.origin_longitude, dset, 'origin_longitude', ('time', ))
+    _create_ncvar(grid.origin_altitude, dset, 'origin_altitude', ('time', ))
 
-    # axes variables
-    _create_ncvar(grid.axes['time'], ncobj, 'time', ('time', ))
-    _create_ncvar(grid.axes['time_end'], ncobj, 'time_end', ('time', ))
-    _create_ncvar(grid.axes['time_start'], ncobj, 'time_start', ('time', ))
-    _create_ncvar(grid.axes['x_disp'], ncobj, 'x_disp', ('nx', ))
-    _create_ncvar(grid.axes['y_disp'], ncobj, 'y_disp', ('ny', ))
-    _create_ncvar(grid.axes['z_disp'], ncobj, 'z_disp', ('nz', ))
-    _create_ncvar(grid.axes['lat'], ncobj, 'lat', ('time', ))
-    _create_ncvar(grid.axes['lon'], ncobj, 'lon', ('time', ))
-    _create_ncvar(grid.axes['alt'], ncobj, 'alt', ('time', ))
+    # write the projection dictionary as a scalar
+    projection = grid.projection.copy()
+    projection['data'] = np.array(1, dtype='int32')
+    # NetCDF does not support boolean attribute, covert to string
+    if '_include_lon_0_lat_0' in projection:
+        include = projection['_include_lon_0_lat_0']
+        projection['_include_lon_0_lat_0'] = ['false', 'true'][include]
+    _create_ncvar(projection, dset, 'projection', ())
+
+    # radar_ attributes
+    radar_attr_names = [
+        'radar_latitude', 'radar_longitude', 'radar_altitude', 'radar_time']
+    for attr_name in radar_attr_names:
+        attr = getattr(grid, attr_name)
+        if attr is not None:
+            _create_ncvar(attr, dset, attr_name, ('nradar', ))
+
+    if grid.radar_name is not None:
+        _create_ncvar(grid.radar_name, dset, 'radar_name',
+                      ('nradar', 'nradar_str_length'))
 
     # create ARM time variables base_time and time_offset, if requested
     if arm_time_variables:
-        time = grid.axes['time']
+        time = grid.time
         dt = netCDF4.num2date(time['data'][0], time['units'])
         td = dt - datetime.datetime.utcfromtimestamp(0)
         td = td.seconds + td.days * 24 * 3600
@@ -164,7 +232,7 @@ def write_grid(filename, grid, format='NETCDF4', arm_time_variables=False):
             'ancillary_variables': 'time_offset',
             'long_name': 'Base time in Epoch',
         }
-        _create_ncvar(base_time, ncobj, 'base_time', ())
+        _create_ncvar(base_time, dset, 'base_time', ())
 
         time_offset = {
             'data': np.array(time['data'], dtype=np.float64),
@@ -173,19 +241,18 @@ def write_grid(filename, grid, format='NETCDF4', arm_time_variables=False):
             'ancillary_variables': 'time_offset',
             'calendar': 'gregorian',
         }
-        _create_ncvar(time_offset, ncobj, 'time_offset', ('time', ))
+        _create_ncvar(time_offset, dset, 'time_offset', ('time', ))
 
     # field variables
     for field, field_dic in grid.fields.items():
         # append 1, to the shape of all data to indicate the time var.
         field_dic['data'].shape = (1, ) + field_dic['data'].shape
-        _create_ncvar(field_dic, ncobj, field, ('time', 'nz', 'ny', 'nx'))
+        _create_ncvar(field_dic, dset, field, ('time', 'nz', 'ny', 'nx'))
         field_dic['data'].shape = field_dic['data'].shape[1:]
 
     # metadata
     for k, v in grid.metadata.items():
-        setattr(ncobj, k, v)
+        setattr(dset, k, v)
 
-    ncobj.close()
-
+    dset.close()
     return
---
+++ pyart/io/tests/test_grid_io.py
@@ -0,0 +1,77 @@
+""" Unit Tests for Py-ART's io/grid_io.py module. """
+
+from __future__ import print_function
+
+from numpy.testing import assert_almost_equal
+
+import pyart
+
+
+def test_grid_write_read():
+    # test the read_grid and write_grid function by performing a
+    # write/read roundtrip and comparing the two Grid objects
+    grid1 = pyart.testing.make_target_grid()
+    grid1.projection['comment'] = 'This is a comment'
+
+    with pyart.testing.InTemporaryDirectory():
+        tmpfile = 'tmp_grid.nc'
+        grid1.write(tmpfile)
+        grid2 = pyart.io.read_grid(tmpfile)
+
+        # check fields
+        for field in grid1.fields.keys():
+            _check_dicts_similar(grid1.fields[field], grid2.fields[field])
+
+        # check attributes
+        _check_attrs_similar(grid1, grid2, 'metadata')
+
+        _check_attrs_similar(grid1, grid2, 'time')
+
+        _check_attrs_similar(grid1, grid2, 'origin_latitude')
+        _check_attrs_similar(grid1, grid2, 'origin_longitude')
+        _check_attrs_similar(grid1, grid2, 'origin_altitude')
+
+        _check_attrs_similar(grid1, grid2, 'regular_x')
+        _check_attrs_similar(grid1, grid2, 'regular_y')
+        _check_attrs_similar(grid1, grid2, 'regular_z')
+
+        _check_attrs_similar(grid1, grid2, 'point_x')
+        _check_attrs_similar(grid1, grid2, 'point_y')
+        _check_attrs_similar(grid1, grid2, 'point_z')
+
+        _check_attrs_similar(grid1, grid2, 'projection')
+        assert grid1.projection['_include_lon_0_lat_0'] is True
+
+        _check_attrs_similar(grid1, grid2, 'point_latitude')
+        _check_attrs_similar(grid1, grid2, 'point_longitude')
+        _check_attrs_similar(grid1, grid2, 'point_altitude')
+
+        assert grid1.nx == grid2.nx
+        assert grid1.ny == grid2.ny
+        assert grid1.nz == grid2.nz
+
+        _check_attrs_similar(grid1, grid2, 'radar_latitude')
+        _check_attrs_similar(grid1, grid2, 'radar_longitude')
+        _check_attrs_similar(grid1, grid2, 'radar_altitude')
+        _check_attrs_similar(grid1, grid2, 'radar_time')
+        _check_attrs_similar(grid1, grid2, 'radar_name')
+        assert grid1.nradar == grid2.nradar
+
+
+def _check_attrs_similar(grid1, grid2, attr):
+    print("Checking attribute:", attr)
+    dic1 = getattr(grid1, attr)
+    dic2 = getattr(grid2, attr)
+    _check_dicts_similar(dic1, dic2)
+
+
+def _check_dicts_similar(dic1, dic2):
+    for k, v in dic1.items():
+        print("Checking key:", k)
+        if k == 'data':
+            if v.dtype.char == 'S':
+                assert ''.join(*v) == ''.join(*dic2[k])
+            else:
+                assert_almost_equal(v, dic2[k])
+        else:
+            assert dic2[k] == v
