9e9a284d502c76b01301791b70644a687586e979
Jonathan Helmus <jjhelmus@gmail.com>
2016-01-18 10:02:28 -0600

    TST: NEXRAD Level 2 message 1 format unit tests

--- pyart/io/tests/test_nexrad_archive_msg1.py
+++ pyart/io/tests/test_nexrad_archive_msg1.py
@@ -1,4 +1,6 @@
-""" Unit Tests for Py-ART's io/nexrad_archive.py module using a MSG31 file. """
+""" Unit Tests for Py-ART's io/nexrad_archive.py module using a MSG1 file. """
+
+import warnings
 
 import numpy as np
 from numpy.testing import assert_almost_equal
@@ -10,8 +12,16 @@ import pyart
 # read_nexrad_archive tests (verify radar attributes) #
 #######################################################
 
-# read in the sample file
-radar = pyart.io.read_nexrad_archive(pyart.testing.NEXRAD_ARCHIVE_MSG31_FILE)
+
+NRAYS = 2567
+NGATES = 1840
+
+
+# read in the sample file, ignore warnings
+with warnings.catch_warnings():
+    warnings.simplefilter('ignore', category=UserWarning)
+    radar = pyart.io.read_nexrad_archive(
+        pyart.testing.NEXRAD_ARCHIVE_MSG1_FILE, station='KLOT')
 
 
 # time attribute
@@ -22,9 +32,9 @@ def test_time():
     assert 'units' in radar.time.keys()
     assert 'calendar' in radar.time.keys()
     assert 'data' in radar.time.keys()
-    assert radar.time['units'] == 'seconds since 2013-07-17T19:50:21Z'
-    assert radar.time['data'].shape == (7200, )
-    assert_almost_equal(radar.time['data'][1], 1, 0)
+    assert radar.time['units'] == 'seconds since 2003-01-01T00:09:21Z'
+    assert radar.time['data'].shape == (NRAYS, )
+    assert_almost_equal(radar.time['data'][1], .506, 3)
 
 
 # range attribute
@@ -36,11 +46,10 @@ def test_range():
     assert 'units' in radar.range
     assert 'data' in radar.range
     assert 'spacing_is_constant' in radar.range
-    assert radar.range['data'].shape == (1832, )
-    assert_almost_equal(radar.range['data'][1], 2375, 0)
-
-
-# fields attribute is tested later
+    assert radar.range['data'].shape == (NGATES, )
+    assert_almost_equal(radar.range['data'][0], -375, 0)
+    assert_almost_equal(radar.range['data'][1], -125, 0)
+    assert_almost_equal(radar.range['data'][-1], 459375, 0)
 
 
 # metadata attribute
@@ -60,7 +69,7 @@ def test_latitude():
     assert 'standard_name' in radar.latitude
     assert 'units' in radar.latitude
     assert radar.latitude['data'].shape == (1, )
-    assert_almost_equal(radar.latitude['data'], 48, 0)
+    assert_almost_equal(radar.latitude['data'], 41.6, 1)
 
 
 # longitude attribute
@@ -69,7 +78,7 @@ def test_longitude():
     assert 'standard_name' in radar.longitude
     assert 'units' in radar.longitude
     assert radar.longitude['data'].shape == (1, )
-    assert_almost_equal(radar.longitude['data'], -122, 0)
+    assert_almost_equal(radar.longitude['data'], -88.1, 1)
 
 
 # altitude attribute
@@ -79,7 +88,7 @@ def test_altitude():
     assert 'units' in radar.altitude
     assert 'positive' in radar.altitude
     assert radar.altitude['data'].shape == (1, )
-    assert_almost_equal(radar.altitude['data'], 195, 0)
+    assert_almost_equal(radar.altitude['data'], 663, 0)
 
 
 # altitude_agl attribute
@@ -90,37 +99,36 @@ def test_altitude_agl():
 # sweep_number attribute
 def test_sweep_number():
     assert 'standard_name' in radar.sweep_number
-    assert np.all(radar.sweep_number['data'] == range(16))
+    assert np.all(radar.sweep_number['data'] == range(7))
 
 
 # sweep_mode attribute
 def test_sweep_mode():
     assert 'standard_name' in radar.sweep_mode
-    assert radar.sweep_mode['data'].shape == (16, )
-    assert radar.sweep_mode['data'].dtype.char == 'S'
-    assert np.all(radar.sweep_mode['data'] == [b'azimuth_surveillance'])
+    assert radar.sweep_mode['data'].shape == (7, )
+    assert np.all(radar.sweep_mode['data'] == ['azimuth_surveillance'])
 
 
 # fixed_angle attribute
 def test_fixed_angle():
     assert 'standard_name' in radar.fixed_angle
     assert 'units' in radar.fixed_angle
-    assert radar.fixed_angle['data'].shape == (16, )
-    assert_almost_equal(radar.fixed_angle['data'][0], 0.48, 2)
+    assert radar.fixed_angle['data'].shape == (7, )
+    assert_almost_equal(radar.fixed_angle['data'][0], 0.5, 1)
 
 
 # sweep_start_ray_index attribute
 def test_sweep_start_ray_index():
     assert 'long_name' in radar.sweep_start_ray_index
-    assert radar.sweep_start_ray_index['data'].shape == (16, )
-    assert_almost_equal(radar.sweep_start_ray_index['data'][0], 0, 0)
+    assert radar.sweep_start_ray_index['data'].shape == (7, )
+    assert_almost_equal(radar.sweep_start_ray_index['data'][1], 367, 0)
 
 
 # sweep_end_ray_index attribute
 def test_sweep_end_ray_index():
     assert 'long_name' in radar.sweep_end_ray_index
-    assert radar.sweep_end_ray_index['data'].shape == (16, )
-    assert_almost_equal(radar.sweep_end_ray_index['data'][0], 719, 0)
+    assert radar.sweep_end_ray_index['data'].shape == (7, )
+    assert_almost_equal(radar.sweep_end_ray_index['data'][1], 733, 0)
 
 
 # target_scan_rate attribute
@@ -134,8 +142,8 @@ def test_azimuth():
     assert 'long_name' in radar.azimuth
     assert 'units' in radar.azimuth
     assert 'axis' in radar.azimuth
-    assert_almost_equal(radar.azimuth['data'][0], 350, 0)
-    assert_almost_equal(radar.azimuth['data'][10], 355, 0)
+    assert_almost_equal(radar.azimuth['data'][0], 245.874, 3)
+    assert_almost_equal(radar.azimuth['data'][10], 255.71777, 3)
 
 
 # elevation attribute
@@ -144,8 +152,8 @@ def test_elevation():
     assert 'long_name' in radar.azimuth
     assert 'units' in radar.elevation
     assert 'axis' in radar.elevation
-    assert radar.elevation['data'].shape == (7200, )
-    assert_almost_equal(radar.elevation['data'][0], 0.75, 2)
+    assert radar.elevation['data'].shape == (NRAYS, )
+    assert_almost_equal(radar.elevation['data'][0], 0.48, 2)
 
 
 # scan_rate attribute
@@ -162,13 +170,13 @@ def test_antenna_transition():
 def test_instument_parameters():
     assert 'nyquist_velocity' in radar.instrument_parameters
     nyq = radar.instrument_parameters['nyquist_velocity']['data']
-    assert_almost_equal(nyq[0], 9, 0)
-    assert_almost_equal(nyq[800], 35, 0)
+    assert_almost_equal(nyq[0], 0, 0)
+    assert_almost_equal(nyq[400], 28.34, 2)
 
     assert 'unambiguous_range' in radar.instrument_parameters
     unamb = radar.instrument_parameters['unambiguous_range']['data']
-    assert_almost_equal(unamb[0], 466000, 0)
-    assert_almost_equal(unamb[800], 117000, 0)
+    assert_almost_equal(unamb[0], 466000., 0)
+    assert_almost_equal(unamb[400], 137000., 0)
 
 
 # radar_calibration attribute
@@ -178,17 +186,17 @@ def test_radar_calibration():
 
 # ngates attribute
 def test_ngates():
-    assert radar.ngates == 1832
+    assert radar.ngates == NGATES
 
 
 # nrays attribute
 def test_nrays():
-    assert radar.nrays == 7200
+    assert radar.nrays == NRAYS
 
 
 # nsweeps attribute
 def test_nsweeps():
-    assert radar.nsweeps == 16
+    assert radar.nsweeps == 7
 
 
 ####################
@@ -196,11 +204,11 @@ def test_nsweeps():
 ####################
 
 
+FIELDS = ['reflectivity', 'spectrum_width', 'velocity']
+
+
 def test_field_dics():
-    fields = [
-        'differential_phase', 'spectrum_width', 'cross_correlation_ratio',
-        'reflectivity', 'differential_reflectivity', 'velocity']
-    for field in fields:
+    for field in FIELDS:
         description = "field : %s, dictionary" % field
         check_field_dic.description = description
         yield check_field_dic, field
@@ -215,25 +223,19 @@ def check_field_dic(field):
 
 
 def test_field_shapes():
-    fields = [
-        'differential_phase', 'spectrum_width', 'cross_correlation_ratio',
-        'reflectivity', 'differential_reflectivity', 'velocity']
-    for field in fields:
+    for field in FIELDS:
         description = "field : %s, shape" % field
         check_field_shape.description = description
         yield check_field_shape, field
 
 
 def check_field_shape(field):
-    assert radar.fields[field]['data'].shape == (7200, 1832)
+    assert radar.fields[field]['data'].shape == (NRAYS, NGATES)
 
 
 def test_field_types():
-    fields = {'differential_phase': MaskedArray,
-              'spectrum_width': MaskedArray,
-              'cross_correlation_ratio': MaskedArray,
+    fields = {'spectrum_width': MaskedArray,
               'reflectivity': MaskedArray,
-              'differential_reflectivity': MaskedArray,
               'velocity': MaskedArray}
     for field, field_type in fields.items():
         description = "field : %s, type" % field
@@ -245,39 +247,68 @@ def check_field_type(field, field_type):
     assert type(radar.fields[field]['data']) is field_type
 
 
-def test_field_first_points():
-    # these values can be found using:
-    # [round(radar.fields[f]['data'][0,0]) for f in radar.fields]
-    fields = {'differential_phase': 181.0,
-              'spectrum_width': np.ma.masked,
-              'cross_correlation_ratio': 0.0,
-              'reflectivity': -32.0,
-              'differential_reflectivity': -8.0,
-              'velocity': np.ma.masked}
-    for field, field_value in fields.items():
-        description = "field : %s, first point" % field
-        check_field_first_point.description = description
-        yield check_field_first_point, field, field_value
+def test_field_data_spectrum_width():
+    assert_almost_equal(radar.fields['spectrum_width']['data'][367, 16:19],
+                        [0.0, 15.5, 16.5])
 
 
-def check_field_first_point(field, value):
-    if np.ma.is_masked(value):
-        assert np.ma.is_masked(radar.fields[field]['data'][0, 0])
-    else:
-        assert_almost_equal(radar.fields[field]['data'][0, 0], value, 0)
+def test_field_data_velocity():
+    assert_almost_equal(radar.fields['velocity']['data'][367, 16:19],
+                        [-17.5, -13.0, 7.5])
 
 
-def test_compressed_archive():
-    # the compressed archive only contains the first 120 radials
+def test_field_data_reflectivity():
+    assert_almost_equal(
+        radar.fields['reflectivity']['data'][0, 8:16],
+        [1.0, 1.0, 0.4375, -0.6875, -1.8125, -2.9375, -3.5, -3.5])
+
+
+def test_field_data_nearest_neighbor():
+    # read in the sample file, ignore warnings
+    with warnings.catch_warnings():
+        warnings.simplefilter('ignore', category=UserWarning)
+        radar = pyart.io.read_nexrad_archive(
+            pyart.testing.NEXRAD_ARCHIVE_MSG1_FILE,
+            station='KLOT', linear_interp=False)
+    assert_almost_equal(
+        radar.fields['reflectivity']['data'][0, 8:16],
+        [1.0, 1.0, 1.0, 1.0, -3.5, -3.5, -3.5, -3.5])
+
+
+def test_field_data_nearest_neighbor():
+    # read in the sample file, ignore warnings
+    with warnings.catch_warnings():
+        warnings.simplefilter('ignore', category=UserWarning)
+        radar = pyart.io.read_nexrad_archive(
+            pyart.testing.NEXRAD_ARCHIVE_MSG1_FILE,
+            station='KLOT', linear_interp=False)
+    assert_almost_equal(
+        radar.fields['reflectivity']['data'][0, 8:16],
+        [1.0, 1.0, 1.0, 1.0, -3.5, -3.5, -3.5, -3.5])
+
+
+def test_single_scan():
     radar = pyart.io.read_nexrad_archive(
-        pyart.testing.NEXRAD_ARCHIVE_MSG31_COMPRESSED_FILE,
-        delay_field_loading=True)
-    assert 'reflectivity' in radar.fields.keys()
-    assert isinstance(radar.fields['reflectivity'],
-                      pyart.lazydict.LazyLoadDict)
-    rdata = radar.fields['reflectivity']['data']
-    assert rdata.shape == (120, 1832)
-    assert_almost_equal(rdata[0, 0], 10.5, 1)
-    assert_almost_equal(rdata[2, 0], 9.5, 1)
-    assert 'velocity' not in radar.fields.keys()
-    assert 'spectrum_width' not in radar.fields.keys()
+        pyart.testing.NEXRAD_ARCHIVE_MSG1_FILE,
+        station='KLOT', scans=[0])
+    assert radar.nrays == 367
+    assert radar.ngates == 460
+    assert radar.nsweeps == 1
+    assert_almost_equal(radar.fields['reflectivity']['data'][0, 6:10],
+                        [19, 26.5, 14.5, 21.5])
+    assert_almost_equal(radar.range['data'][0], 0, 0)
+    assert_almost_equal(radar.range['data'][1], 1000, 0)
+
+
+def test_only_ref():
+    radar = pyart.io.read_nexrad_archive(
+        pyart.testing.NEXRAD_ARCHIVE_MSG1_FILE,
+        station='KLOT', scans=[0, 2, 4, 5, 6],
+        exclude_fields=['velocity', 'spectrum_width'])
+    assert_almost_equal(radar.range['data'][0], 0, 0)
+    assert_almost_equal(radar.range['data'][1], 1000, 0)
+    assert_almost_equal(radar.fields['reflectivity']['data'][0, 6:10],
+                        [19, 26.5, 14.5, 21.5])
+    assert radar.nrays == 367 + 368 + 366 + 366 + 366
+    assert radar.ngates == 460
+    assert radar.nsweeps == 5
