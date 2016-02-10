89db23528a2eea0ad807309b5157f49257f8af74
Jonathan Helmus <jjhelmus@gmail.com>
2016-02-10 10:31:00 -0600

    MAINT: Remove Python 2.6, 3.3 and add 3.5 to tags

--- setup.py
+++ setup.py
@@ -35,11 +35,10 @@ Intended Audience :: Developers
 License :: OSI Approved :: BSD License
 Programming Language :: Python
 Programming Language :: Python :: 2
-Programming Language :: Python :: 2.6
 Programming Language :: Python :: 2.7
 Programming Language :: Python :: 3
-Programming Language :: Python :: 3.3
 Programming Language :: Python :: 3.4
+Programming Language :: Python :: 3.5
 Programming Language :: C
 Programming Language :: Cython
 Programming Language :: Fortran
