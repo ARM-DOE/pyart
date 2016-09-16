1deb6fd842fdc912d7ce353e2009239dc9f7c20f
Jonathan Helmus <jjhelmus@gmail.com>
2016-09-16 13:14:35 -0500

    DEP: Remove deprecated RadarDisplay_Airborne class
    Class is now named AirborneRadarDisplay

--- pyart/graph/__init__.py
+++ pyart/graph/__init__.py
@@ -33,17 +33,4 @@ from .radardisplay_airborne import AirborneRadarDisplay
 from .gridmapdisplay import GridMapDisplay
 from .radarmapdisplay import RadarMapDisplay
 
-import warnings as _warnings
-
-
-class RadarDisplay_Airborne(AirborneRadarDisplay):
-    """ Deprecated name for the AirborneRadarDisplay class. """
-
-    def __init__(self, *args, **kwargs):
-        _warnings.warn(
-            ("'RadarDisplay_Airborne' is deprecated and will be removed in"
-             "future versions of Py-ART, please use 'AirborneRadarDisplay'"),
-            DeprecationWarning)
-        AirborneRadarDisplay.__init__(self, *args, **kwargs)
-
 __all__ = [s for s in dir() if not s.startswith('_')]
