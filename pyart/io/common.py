a37cac9aa933984e686bfbe282923e0f2d1214aa
Jonathan Helmus <jjhelmus@gmail.com>
2016-09-16 13:27:52 -0500

    DEP: Remove deprecated non-public functions
    Remove to pyart.io.common.radar_coords_to_cart,
    pyart.graph.common.radar_coords_to_cart and pyart.graph.sweep_coords_to_cart
    functions.

--- pyart/io/common.py
+++ pyart/io/common.py
@@ -20,13 +20,6 @@ import gzip
 import numpy as np
 import netCDF4
 
-# Deprecated function names in this name space
-from ..exceptions import _deprecated_alias
-from ..core import transforms as _transforms
-radar_coords_to_cart = _deprecated_alias(
-    _transforms.antenna_to_cartesian, 'pyart.io.common.radar_coords_to_cart',
-    'pyart.core.transforms.antenna_to_cartesian')
-
 
 def prepare_for_read(filename):
     """
