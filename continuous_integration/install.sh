ff2d6b020bc0d9fc4dda133a6782c1d45af48944
Jonathan Helmus <jjhelmus@gmail.com>
2017-03-04 12:31:12 -0600

    TST: Do no use jjhelmus channel for Travis CI

--- continuous_integration/install.sh
+++ continuous_integration/install.sh
@@ -17,8 +17,6 @@ conda update -q conda
 ## Create a testenv with the correct Python version
 conda env create -f continuous_integration/environment-$PYTHON_VERSION.yml
 source activate testenv
-# KLUDGE to replace unwanted packages from the jjhelmus channel
-conda install -c conda-forge pyproj wradlib
 # TODO install cbc, cylp, glpk, cvxopt_glpk in Python 2.7
 
 # install coverage modules
