752e4f6b046e3ded2d48ba7c01b76b3a9b28d387
Zachary Sherman <zsherman@anl.gov>
2018-02-15 12:54:35 -0600

    REL: Release 1.9.0 Picasso

--- setup.py
+++ setup.py
@@ -72,7 +72,7 @@ PLATFORMS = ["Linux", "Mac OS-X", "Unix"]
 MAJOR = 1
 MINOR = 9
 MICRO = 0
-ISRELEASED = False
+ISRELEASED = True
 VERSION = '%d.%d.%d' % (MAJOR, MINOR, MICRO)
 SCRIPTS = glob.glob('scripts/*')
 
