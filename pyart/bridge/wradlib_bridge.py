94c0ac1d240ba1b6bc37efbf36d2f7c25fd2acc5
Robert Jackson <rjackson@anl.gov>
2018-03-26 13:27:51 -0500

    FIX: Issue with phidp field in texture_of_complex_phase

--- pyart/bridge/wradlib_bridge.py
+++ pyart/bridge/wradlib_bridge.py
@@ -60,13 +60,15 @@ def texture_of_complex_phase(radar, phidp_field=None,
         raise MissingOptionalDependency(
             "wradlib is required to use texture_of_complex_phase but is " +
             "not installed")
-
+            
     # parse field names
     if phidp_field is None:
         phidp_field = get_field_name('differential_phase')
+        
     if phidp_texture_field is None:
-        phidp_field = get_field_name('differential_phase')
+        phidp_texture_field = get_field_name('differential_phase')
 
+    
     # Grab the phase data
     phidp = radar.fields[phidp_field]['data']
 
@@ -79,4 +81,6 @@ def texture_of_complex_phase(radar, phidp_field=None,
 
     texture_field = get_metadata(phidp_texture_field)
     texture_field['data'] = w_texture_complex
+    texture_field['standard_name'] = 'texture_of_differential_phase_hv'
+    texture_field['long_name'] = 'Texture of differential phase'
     return texture_field
