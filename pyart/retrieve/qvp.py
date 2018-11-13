462ac1f09e3b971de24e04d11587dc9f484b8273
jhemedin <jason.hemedinger@valpo.edu>
2018-11-12 18:09:33 -0600

    MAINT: fixed non-ascii error

--- pyart/retrieve/qvp.py
+++ pyart/retrieve/qvp.py
@@ -1,4 +1,4 @@
-"""
+﻿"""
 pyart.retrieve.quasi_vertical_profile
 =====================================
 
@@ -49,17 +49,17 @@ def quasi_vertical_profile(radar, fields = None, gatefilter = None):
     
     References
     ----------
-    Troemel, S., M. Kumjian, A. Ryzhkov, and C. Simmer, 2013: Backscatter 
-    differential phase – estimation and variability. J. Appl. Meteor. Clim.. 
-    52, 2529 – 2548.
+    Troemel, S., M. Kumjian, A. Ryzhkov, and C. Simmer, 2013: Backscatter
+    differential phase - estimation and variability. J Appl. Meteor. Clim..
+    52, 2529 - 2548.
 
-    Troemel, S., A. Ryzhkov, P. Zhang, and C. Simmer, 2014: Investigations 
-    of backscatter differential phase in the melting layer. J. Appl. Meteorol. 
-    Clim. 53, 2344 – 2359.
+    Troemel, S., A. Ryzhkov, P. Zhang, and C. Simmer, 2014: Investigations
+    of backscatter differential phase in the melting layer. J. Appl. Meteorol.
+    Clim. 54, 2344 - 2359.
 
-    Ryzhkov, A., P. Zhang, H. Reeves, M. Kumjian, T. Tschallener, S. Troemel, 
-    C. Simmer, 2015: Quasi-vertical profiles – a new way to look at polarimetric 
-    radar data. Submitted to J. Atmos. Oceanic Technol.
+    Ryzhkov, A., P. Zhang, H. Reeves, M. Kumjian, T. Tschallener, S. Tromel, 
+    C. Simmer, 2015: Quasi-vertical profiles - a new way to look at polarimetric
+    radar data. Submitted to J. Atmos. Oceanic Technol. 
     
     """
     
