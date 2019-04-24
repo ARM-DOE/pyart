36f715bef253ceeb4f31db0f4812869f61144cd4
jhemedin <jason.hemedinger@gmail.com>
2019-04-24 13:09:12 -0500

    STY: removed jupyter markings, extra whitespace, and added spaces in comments

--- examples/plotting/plot_three_panel_gridmapdisplay.py
+++ examples/plotting/plot_three_panel_gridmapdisplay.py
@@ -1,9 +1,3 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[18]:
-
-
 """
 ===========================================
 Create a 3 pannel plot using GridMapDisplay
@@ -16,20 +10,20 @@ and longitude slice using xarray and a cartopy background
 
 print(__doc__)
 
-#Author: Jason Hemedinger
-#License: BSD 3 clause
+# Author: Jason Hemedinger
+# License: BSD 3 clause
 
 import numpy as np
 import matplotlib.pyplot as plt
 import cartopy.crs as ccrs
 import pyart
 
-#Read in the gridded file, create GridMapDisplay object
+# Read in the gridded file, create GridMapDisplay object
 filename = '20110520100000_nexrad_grid.nc'
 radar = pyart.io.read_grid(filename)
 display = pyart.graph.GridMapDisplay(radar)
 
-#Setting projection, figure size, and panel sizes.
+# Setting projection, figure size, and panel sizes.
 projection = ccrs.PlateCarree()
 
 fig = plt.figure(figsize=[15,7])
@@ -38,21 +32,21 @@ map_panel_axes = [0.05, 0.05, .4, .80]
 x_cut_panel_axes = [0.55, 0.10, .4, .25]
 y_cut_panel_axes = [0.55, 0.50, .4, .25]
 
-#Set parameters.
+# Set parameters.
 level = 1
 vmin = -8
 vmax = 64
 lat = 36.5
 lon = -97.7
 
-#Panel 1: PPI plot of the second tilt.
+# Panel 1: PPI plot of the second tilt.
 ax1 = fig.add_axes(map_panel_axes, projection=projection)
 display.plot_grid('REF', 1, vmin=vmin, vmax=vmax,
                   projection=projection,
                   cmap='pyart_HomeyerRainbow')
 display.plot_crosshairs(lon=lon, lat=lat)
 
-#Panel 2: longitude slice
+# Panel 2: longitude slice
 ax2 = fig.add_axes(x_cut_panel_axes)
 display.plot_longitude_slice('REF', lon=lon, lat=lat,
                              vmin=vmin, vmax=vmax,
@@ -71,10 +65,3 @@ ax3.set_xlim([-50, 50])
 
 plt.show()
 plt.close()
-
-
-# In[ ]:
-
-
-
-
