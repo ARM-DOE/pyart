16343596aeb130db2cd25b22e871e2d711a7de8f
zssherman <shermanx1991@gmail.com>
2020-01-29 13:57:33 -0500

    DOC: Adding mention of wradlib in __init__

--- pyart/bridge/__init__.py
+++ pyart/bridge/__init__.py
@@ -3,6 +3,11 @@ Py-ART can act as bridge to other community software projects.
 
 The functionality in this namespace is available in other pyart namespaces.
 
+Current extensions:
+    * wradlib https://wradlib.org/
+
+
+
 """
 
 from .wradlib_bridge import texture_of_complex_phase
