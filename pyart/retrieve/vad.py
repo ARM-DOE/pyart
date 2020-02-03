fb1ec2f9d980b40785c519ec3dbe32c507109ea4
zssherman <zsherman@anl.gov>
2020-02-03 12:17:38 -0600

    DOC: Fixed incorrect section title reference.

--- pyart/retrieve/vad.py
+++ pyart/retrieve/vad.py
@@ -37,8 +37,8 @@ def velocity_azimuth_display(radar, vel_field=None, z_want=None,
 	A velocity azimuth display object containing height, speed, direction,
         u_wind, v_wind from a radar object.
 
-    Reference
-    ---------
+    References
+    ----------
     Michelson, D. B., Andersson, T., Koistinen, J., Collier, C. G., Riedl, J.,
     Szturc, J., Gjertsen, U., Nielsen, A. and Overgaard, S. (2000) BALTEX Radar
     Data Centre Products and their Methodologies. In SMHI Reports. Meteorology
