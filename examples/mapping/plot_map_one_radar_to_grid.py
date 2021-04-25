cd82f717a13796b87291c93a86b8af4a2bb50970
Quba1 <jak.lewand@gmail.com>
2021-04-25 13:36:09 +0200

    Update plot_map_one_radar_to_grid.py
    typo correction

--- examples/mapping/plot_map_one_radar_to_grid.py
+++ examples/mapping/plot_map_one_radar_to_grid.py
@@ -20,7 +20,7 @@ import pyart
 RADAR_FILE = '110635.mdv'
 radar = pyart.io.read_mdv(RADAR_FILE)
 
-# mask out last 10 gates of each ray, this removes the "ring" around th radar.
+# mask out last 10 gates of each ray, this removes the "ring" around the radar.
 radar.fields['reflectivity']['data'][:, -10:] = np.ma.masked
 
 # exclude masked gates from the gridding
