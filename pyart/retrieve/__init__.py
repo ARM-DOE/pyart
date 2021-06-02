f35227eba5ae31b06d42f6551691ecebbe0cd11e
zssherman <shermanx1991@gmail.com>
2021-06-02 13:43:44 -0500

    ENH: Adding browning vad code as a replacement. Added browning vad code as a addition to vad suite as well as a temp replacement for the vad code that is having incorrect results as I continue working on a solution.

--- pyart/retrieve/__init__.py
+++ pyart/retrieve/__init__.py
@@ -16,7 +16,7 @@ from .advection import grid_displacement_pc, grid_shift
 from .qpe import est_rain_rate_zpoly, est_rain_rate_z, est_rain_rate_kdp
 from .qpe import est_rain_rate_a, est_rain_rate_zkdp, est_rain_rate_za
 from .qpe import est_rain_rate_hydro
-from .vad import velocity_azimuth_display
+from .vad import vad_michelson, vad_browning
 from .qvp import quasi_vertical_profile
 
 __all__ = [s for s in dir() if not s.startswith('_')]
