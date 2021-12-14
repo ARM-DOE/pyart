28d791d1a9333b39b397f536bd6805d66d9a866e
Robert Jackson <rjackson@anlvpn104.evs.anl.gov>
2021-12-14 10:54:33 -0600

    ADD: Tests for functionality.

--- pyart/map/__init__.py
+++ pyart/map/__init__.py
@@ -4,11 +4,11 @@ coordinates to Cartesian coordinates.
 
 """
 
+from .gate_mapper import GateMapper
 from .grid_mapper import map_to_grid, grid_from_radars
 from .grid_mapper import example_roi_func_constant
 from .grid_mapper import example_roi_func_dist
 from .grid_mapper import example_roi_func_dist_beam
 from .gates_to_grid import map_gates_to_grid
-from .gatemapper import GateMapper
 
 __all__ = [s for s in dir() if not s.startswith('_')]
