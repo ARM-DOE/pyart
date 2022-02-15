ad19e17152937c6ca9476a820617dd91434ae381
lauratomkins <lmtomkin@ncsu.edu>
2022-02-15 15:34:22 -0500

    Changed name of muted radar object

--- pyart/util/tests/test_image_mute.py
+++ pyart/util/tests/test_image_mute.py
@@ -6,18 +6,18 @@ import numpy as np
 def test_image_mute_radar():
     # read in example file and image mute
     radar = pyart.io.read_nexrad_archive(pyart.testing.NEXRAD_ARCHIVE_MSG31_FILE)
-    radar = pyart.util.image_mute_radar(radar, field='reflectivity',
+    mute_radar = pyart.util.image_mute_radar(radar, field='reflectivity',
                                         mute_field='cross_correlation_ratio', mute_threshold=0.97)
 
     # check that fields are added to the radar object
-    assert 'nonmuted_reflectivity' in radar.fields.keys()
-    assert 'muted_reflectivity' in radar.fields.keys()
+    assert 'nonmuted_reflectivity' in mute_radar.fields.keys()
+    assert 'muted_reflectivity' in mute_radar.fields.keys()
 
     # check that the number of points in the muted and non muted reflectivity
     # fields have the same number of points as the rhoHV field
-    n_rhohv = np.sum(~radar.fields['cross_correlation_ratio']['data'].mask)
-    n_mutez = np.sum(~radar.fields['muted_reflectivity']['data'].mask)
-    n_nonmutez = np.sum(~radar.fields['nonmuted_reflectivity']['data'].mask)
+    n_rhohv = np.sum(~mute_radar.fields['cross_correlation_ratio']['data'].mask)
+    n_mutez = np.sum(~mute_radar.fields['muted_reflectivity']['data'].mask)
+    n_nonmutez = np.sum(~mute_radar.fields['nonmuted_reflectivity']['data'].mask)
 
     assert n_mutez + n_nonmutez == n_rhohv
 
