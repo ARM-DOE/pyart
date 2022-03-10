86e4d81117a3b6b8d6b3584ba5ad05f7b4923721
mgrover1 <maxi2312@gmail.com>
2022-03-10 15:29:30 -0600

    remove rtd artifcats

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -140,15 +140,15 @@ todo_include_todos = False
 #
 
 html_theme = 'pydata_sphinx_theme'
-import sphinx_rtd_theme
-html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
+import pydata_sphinx_theme
+html_theme_path = [pydata_sphinx_theme.get_html_theme_path()]
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
 # documentation.
 #
 html_theme_options = {
-    'analytics_id': 'UA-221367849-1',
+    'google_analytics_id': 'UA-221367849-1',
 }
 
 # Add any paths that contain custom static files (such as style sheets) here,
