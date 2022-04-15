8f57a0d144f32fdcb3f4db69bc60ee20e3074b23
mgrover1 <maxi2312@gmail.com>
2022-04-15 15:32:39 -0500

    check to see if ncvar is a dataarray

--- pyart/io/cfradial.py
+++ pyart/io/cfradial.py
@@ -342,8 +342,13 @@ def _find_all_meta_group_vars(ncvars, meta_group_name):
 def _ncvar_to_dict(ncvar, lazydict=False):
     """ Convert a NetCDF Dataset variable to a dictionary. """
     # copy all attribute except for scaling parameters
-    d = dict((k, getattr(ncvar, k)) for k in ncvar.attrs
-             if k not in ['scale_factor', 'add_offset'])
+
+    if type(ncvar) == xr.DataArray:
+        d = dict((k, getattr(ncvar, k)) for k in ncvar.attrs
+                if k not in ['scale_factor', 'add_offset'])
+    else:
+        d = dict((k, getattr(ncvar, k)) for k in ncvar.attrs
+                if k not in ['scale_factor', 'add_offset'])
     data_extractor = _NetCDFVariableDataExtractor(ncvar)
     if lazydict:
         d = LazyLoadDict(d)
