1e9536349d77df062a39236ef07e980171e91746
jrobrien91 <obrienj@anl.gov>
2022-04-25 11:39:38 -0500

    ENH: PyART doc static image color schemes changed to colorblind friendly PyART scheme. plot_ppi_mdv.py modified for this change. plot_rhi_cfradial_singlescan.py created to create one rhi cfradial plot for the docs.

--- examples/plotting/plot_ppi_mdv.py
+++ examples/plotting/plot_ppi_mdv.py
@@ -21,11 +21,8 @@ filename = get_test_data('110635.mdv')
 radar = pyart.io.read_mdv(filename)
 display = pyart.graph.RadarDisplay(radar)
 fig = plt.figure(figsize=[5, 5])
-ax = fig.add_subplot(111, frameon=False)
-display.plot('reflectivity', 0, vmin=-16., vmax=64,
-             mask_tuple=['normalized_coherent_power', 0.5],
-             colorbar_flag=False, title_flag=False,
-             axislabels_flag=False)
-display.set_limits(ylim=[-120, 120], xlim=[-120, 120])
-fig.subplots_adjust(left=0, bottom=0, right=1, top=1, wspace=0, hspace=0)
+ax = fig.add_subplot(111)
+display.plot('reflectivity', 0, vmin=-16., vmax=64, title="PPI",
+              cmap='pyart_HomeyerRainbow')
+display.set_limits(ylim=[-150, 150], xlim=[-150, 150])
 plt.show()
