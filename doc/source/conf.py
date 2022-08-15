795d6f03aeeb034583f948b83a7b0b7edb3decb1
mgrover1 <mgroverwx@gmail.com>
2022-08-15 11:52:50 -0500

    remove appending module list

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -25,7 +25,7 @@ import sys
 # cwd = Path.cwd().resolve()
 print(sys.path)
 # sys.path.insert(0, os.path.abspath("."))
-sys.path.insert(0, os.path.abspath('../../'))
+# sys.path.insert(0, os.path.abspath('../../'))
 print(sys.path)
 
 
