1eae1c440f79a945d6d1ccb30f2ff6e835fd9352
mgrover1 <mgroverwx@gmail.com>
2022-08-24 13:37:17 -0500

    add tests for xradar

---
+++ pyart/core/xradar.py
@@ -0,0 +1,63 @@
+import numpy as np
+from datatree import DataTree
+
+def create_dataset_from_sweep(radar, sweep=0, field=None):
+    """
+    Creates an xarray.Dataset from sweeps
+    """
+    
+    # Grab the range
+    range = radar.range['data']
+    
+    # Grab the elevation
+    elevation = radar.get_elevation(sweep)
+    
+    # Grab the azimuth
+    azimuth = radar.get_azimuth(sweep)
+    
+    # Grab the x, y, z values
+    x, y, z = radar.get_gate_x_y_z(sweep)
+    
+    # Grab the lat, lon, and elevation
+    lat, lon, alt = radar.get_gate_lat_lon_alt(sweep)
+    
+    # Add the fields
+    field_dict = {}
+    for field in list(radar.fields):
+        field_dict[field]=(["azimuth", "range"], radar.get_field(sweep, field))
+    
+    ds = xr.Dataset(
+        data_vars=field_dict,
+         coords=dict(
+             x=(["azimuth", "range"], x),
+             y=(["azimuth", "range"], y),
+             z=(["azimuth", "range"], z),
+             lat=(["azimuth", "range"], lat),
+             lon=(["azimuth", "range"], lon),
+             alt=(["azimuth", "range"], alt),
+             azimuth=azimuth,
+             elevation=elevation,
+             sweep=np.array([sweep]),
+             range=range),
+    )
+    
+    return ds.chunk()
+
+
+def convert_to_xradar(radar):
+    """
+    Converts from radar to xradar
+    """
+    
+    ds_list = []
+    sweeps = radar.sweep_number['data']
+    for sweep in sweeps:
+        ds_list.append(create_dataset_from_sweep(radar, sweep))
+    
+    # Convert the numpy array to a list of strings
+    dict_keys = [x for x in list(sweeps.astype(str))]
+    
+    # Zip the keys and datasets and turn into a dictionary
+    dict_of_dsets = dict(zip(dict_keys, ds_list))
+    
+    return DataTree.from_dict(dict_of_dsets)
---
+++ tests/core/test_xradar.py
@@ -0,0 +1,13 @@
+import pyart
+import xarray as xr
+
+# Load the sample radar data
+test_radar = pyart.io.read(pyart.testing.get_test_data('110635.mdv'))
+
+def test_create_dataset_from_sweep(test_radar=test_radar):
+    xradar_single_sweep = pyart.core.xradar.create_dataset_from_sweep(test_radar)
+    assert isinstance(xradar_single_sweep, xr.Dataset)
+
+def test_convert_to_xradar(test_radar=test_radar):
+    xradar_dataset = pyart.core.xradar.convert_to_xradar(test_radar)
+    assert isinstance(xradar_dataset, xc.Collection)
