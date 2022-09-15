6289c0547ed521c6542ac604182abf29d51def93
Max Grover <maxi2312@gmail.com>
2022-09-15 10:56:27 -0500

    make datetime parsing more general for columnsect (#1261)

--- pyart/util/columnsect.py
+++ pyart/util/columnsect.py
@@ -202,8 +202,8 @@ def get_field_location(radar, latitude, longitude):
 
     # Determine the time at the center of each ray within the column
     # Define the start of the radar volume as a numpy datetime object for xr
-    base_time = pd.to_datetime(radar.time['units'].split(' ')[-1].strip('Z'),
-                               format='%Y-%m-%dT%H:%M:%S').to_numpy()
+    # We take advantage of the "seconds since " portion of the units string
+    base_time = pd.to_datetime(radar.time['units'][14:]).to_numpy()
     # Convert Py-ART radar object time (time since volume start) to time delta
     # Add to base time to have sequential time within the xr Dataset
     # for easier future merging/work
