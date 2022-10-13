867475d1742c6cc5c8e4adbb2733d79fa0eebb79
mgrover1 <mgrover1@users.noreply.github.com>
2022-10-13 13:02:14 +0000

    deploy: 68306c9c175262e6ee40cd1864cffad7763c7cca

---
+++ _sources/API/generated/pyart.graph.GridMapDisplay.plot_cross_section.rst.txt
@@ -0,0 +1,6 @@
+pyart.graph.GridMapDisplay.plot\_cross\_section
+===============================================
+
+.. currentmodule:: pyart.graph
+
+.. automethod:: GridMapDisplay.plot_cross_section
--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '1.13.2.post4',
+    VERSION: '1.13.2.post5',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
