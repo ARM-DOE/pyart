41a6d7dfa5e2447dc779ec3614a7aa4f1829103b
mgrover1 <mgroverwx@gmail.com>
2023-09-29 10:23:22 -0500

    FIX: Use allclose instead of assert equal

--- tests/xradar/test_accessor.py
+++ tests/xradar/test_accessor.py
@@ -1,6 +1,6 @@
 import numpy as np
 import xradar as xd
-from numpy.testing import assert_almost_equal
+from numpy.testing import assert_allclose
 from open_radar_data import DATASETS
 
 import pyart
@@ -54,5 +54,7 @@ def test_grid(filename=filename):
         grid_limits=((2000, 2000), (-100_000.0, 100_000.0), (-100_000.0, 100_000.0)),
         fields=["DBZ"],
     )
-    assert_almost_equal(grid.x["data"], np.arange(-100_000, 120_000, 20_000))
-    assert_almost_equal(grid.fields["DBZ"]["data"][0, -1, 0], np.array(0.4243435))
+    assert_allclose(grid.x["data"], np.arange(-100_000, 120_000, 20_000))
+    assert_allclose(
+        grid.fields["DBZ"]["data"][0, -1, 0], np.array(0.4243435), rtol=1e-03
+    )
