FIX: physically consistent RoI calculation for gridding; updated gridding input parameters to mitigate smearing (#1552)
* ENH: Dimension-dependent weighting factor flexibility in gridding
(previously, weighting scaling was optional only for the z-dim)

* TST: modify tests to align with new mapping scaling factor flexibility

* FIX: dist2 bug fix + STY

* FIX: optional conversion of  h_factor and dist_factor to array (compatibility)

* FIX: physically consistent RoI calculation for gridding; updated gridding input parameters to mitigate smearing

* ENH: `min_radius` smaller for ARM radars reflecting higher resolution
FIX: consistent default values in `map_to_grid`

* STY: liniting (black)

* Increase min_radius default to necessarily cover 250 m resolution fluctuations

* FIX: `try` statement for using/testing metadata information

* TST: update a few tests which were to rigid without flexibility to consider any changes to the gridding methods

* STY: linting (black)

* FIX: specific AttributeError; explicit default input parameter values

pyart/map/gates_to_grid.py
@@ -35,9 +35,9 @@ def map_gates_to_grid(
     constant_roi=None,
     z_factor=0.05,
     xy_factor=0.02,
-    min_radius=500.0,
+    min_radius=250.0,
     h_factor=(1.0, 1.0, 1.0),
-    nb=1.5,
+    nb=1.0,
     bsp=1.0,
     dist_factor=(1.0, 1.0, 1.0),
     **kwargs
@@ -94,6 +94,19 @@ def map_gates_to_grid(
 
     if len(radars) == 0:
         raise ValueError("Length of radars tuple cannot be zero")
+
+    # set min_radius depending on whether processing ARM radars
+    try:
+        if "platform_id" in radars[0].metadata.keys():
+            if np.any(
+                [
+                    x in radars[0].metadata["platform_id"].lower()
+                    for x in ["sacr", "sapr"]
+                ]
+            ):
+                min_radius = 100.0
+    except AttributeError:
+        pass
 
     skip_transform = False
     if len(radars) == 1 and grid_origin_alt is None and grid_origin is None:

pyart/map/grid_mapper.py
@@ -292,9 +292,9 @@ def map_to_grid(
     constant_roi=None,
     z_factor=0.05,
     xy_factor=0.02,
-    min_radius=500.0,
-    h_factor=1.0,
-    nb=1.5,
+    min_radius=250.0,
+    h_factor=(1.0, 1.0, 1.0),
+    nb=1.0,
     bsp=1.0,
     **kwargs
 ):
@@ -394,8 +394,10 @@ def map_to_grid(
     h_factor, nb, bsp, min_radius : float
         Radius of influence parameters for the built in 'dist_beam' function.
         The parameter correspond to the height scaling, virtual beam width,
-        virtual beam spacing, and minimum radius of influence. These
-        parameters are only used when `roi_func` is 'dist_mean'.
+        virtual beam spacing, and minimum radius of influence.
+        NOTE: the default `min_radius` value is smaller for ARM radars
+        to reflect their higher resolution relative to precipitation radars..
+        These parameters are only used when `roi_func` is 'dist_mean'.
     copy_field_data : bool
         True to copy the data within the radar fields for faster gridding,
         the dtype for all fields in the grid will be float64. False will not
@@ -435,6 +437,19 @@ def map_to_grid(
 
     if len(radars) == 0:
         raise ValueError("Length of radars tuple cannot be zero")
+
+    # set min_radius depending on whether processing ARM radars
+    try:
+        if "platform_id" in radars[0].metadata.keys():
+            if np.any(
+                [
+                    x in radars[0].metadata["platform_id"].lower()
+                    for x in ["sacr", "sapr"]
+                ]
+            ):
+                min_radius = 100.0
+    except AttributeError:
+        pass
 
     skip_transform = False
     if len(radars) == 1 and grid_origin_alt is None and grid_origin is None:

tests/xradar/test_accessor.py
@@ -81,9 +81,7 @@ def test_grid(filename=filename):
         fields=["DBZ"],
     )
     assert_allclose(grid.x["data"], np.arange(-100_000, 120_000, 20_000))
-    assert_allclose(
-        grid.fields["DBZ"]["data"][0, -1, 0], np.array(0.4243435), rtol=1e-03
-    )
+    assert_allclose(grid.fields["DBZ"]["data"][0, -1, 0], np.array(-0.511), rtol=1e-03)
 
 
 def _check_attrs_similar(grid1, grid2, attr):