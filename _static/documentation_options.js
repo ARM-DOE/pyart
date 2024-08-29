26d7dcdd70b47cbdb3103caf5b9007b80c84d30f
mgrover1 <mgrover1@users.noreply.github.com>
2024-08-29 21:27:31 +0000

    deploy: 6f9876977d1a65388769ae0995e5307030d85025

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '1.18.7.post7',
+    VERSION: '1.18.7.post8',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
