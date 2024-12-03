564e81b3aee43c1e178d41f9beb5c4ef1fed1413
mgrover1 <mgroverwx@gmail.com>
2024-12-03 14:56:16 -0600

    ADD: Add cfradial2 example

---
+++ examples/io/plot_read_cfradial2.py
@@ -0,0 +1,28 @@
+"""
+==========================================================
+Read and Plot Cfradial2/FM301 data Using Xradar and Py-ART
+==========================================================
+
+An example which uses xradar and Py-ART to read and plot Cfradial2/FM301 data.
+
+"""
+
+# Author: Max Grover (mgrover@anl.gov)
+# License: BSD 3 clause
+
+
+import xradar as xd
+
+import pyart
+from pyart.testing import get_test_data
+
+# Locate the test data and read in using xradar
+filename = get_test_data("cfrad2.20080604_002217_000_SPOL_v36_SUR.nc")
+tree = xd.io.open_cfradial1_datatree(filename)
+
+# Give the tree Py-ART radar methods
+radar = tree.pyart.to_radar()
+
+# Plot the Reflectivity Field (corrected_reflectivity_horizontal)
+display = pyart.graph.RadarMapDisplay(radar)
+display.plot_ppi("DBZ", cmap="pyart_ChaseSpectral", vmin=-20, vmax=70)
