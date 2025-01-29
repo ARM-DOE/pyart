386b807c607fd506fc2d518f480420400a5af261
gewitterblitz <milindsharma@tamu.edu>
2025-01-28 23:13:11 -0600

    update skiprows parameter to avoid losing data

--- pyart/io/nexrad_common.py
+++ pyart/io/nexrad_common.py
@@ -39,41 +39,52 @@ def get_nexrad_location(station):
 # https://www.ncei.noaa.gov/access/homr/
 
 # Read the nexrad-stations.txt file directly from the URL
-nexrad_station_file = 'https://www.ncei.noaa.gov/access/homr/file/nexrad-stations.txt;jsessionid=EC5C4FA497A46EFAF53FD6068F870D3E'
+nexrad_station_file = "https://www.ncei.noaa.gov/access/homr/file/nexrad-stations.txt;jsessionid=EC5C4FA497A46EFAF53FD6068F870D3E"
 
 # Define the column specifications
 colspecs = [
-    (0, 8),   # NCDCID
+    (0, 8),  # NCDCID
     (9, 13),  # ICAO
-    (14, 19), # WBAN
-    (20, 50), # NAME
-    (51, 71), # COUNTRY
-    (72, 74), # ST
-    (75, 105),# COUNTY
-    (106, 115),# LAT
-    (116, 126),# LON
-    (127, 133),# ELEV
-    (134, 139),# UTC
-    (140, 190) # STNTYPE
+    (14, 19),  # WBAN
+    (20, 50),  # NAME
+    (51, 71),  # COUNTRY
+    (72, 74),  # ST
+    (75, 105),  # COUNTY
+    (106, 115),  # LAT
+    (116, 126),  # LON
+    (127, 133),  # ELEV
+    (134, 139),  # UTC
+    (140, 190)   # STNTYPE
 ]
 
 # Define the column names
 colnames = [
-    'NCDCID', 'ICAO', 'WBAN', 'NAME', 'COUNTRY', 'ST', 'COUNTY', 
-    'LAT', 'LON', 'ELEV', 'UTC', 'STNTYPE'
+    "NCDCID",
+    "ICAO",
+    "WBAN",
+    "NAME", 
+    "COUNTRY", 
+    "ST", 
+    "COUNTY", 
+    "LAT", 
+    "LON", 
+    "ELEV", 
+    "UTC", 
+    "STNTYPE",
 ]
 
 # Read the fixed-width file
-df_nexrad_loc = pd.read_fwf(nexrad_station_file, colspecs=colspecs, names=colnames, skiprows=6)
+df_nexrad_loc = pd.read_fwf(
+    nexrad_station_file, colspecs=colspecs, names=colnames, skiprows=2
+)
 
 
 # Create the dictionary
 NEXRAD_LOCATIONS = {
-    row['ICAO']: {
-        'lat': float(row['LAT']),
-        'lon': float(row['LON']),
-        'elev': int(row['ELEV'])
+    row["ICAO"]: {
+        "lat": float(row["LAT"]),
+        "lon": float(row["LON"]),
+        "elev": int(row["ELEV"]),
     }
     for _, row in df_nexrad_loc.iterrows()
 }
-
