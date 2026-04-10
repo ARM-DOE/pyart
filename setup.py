fd3a7ee4d0739143aeb77ba951b02e030556e6a9
Joe O'Brien <obrienj@anl.gov>
2026-04-10 16:55:25 -0500

    DOC: Adding correct reference to ARM  (#1846)
    * DOC: Update ARM reference to User Facility
    
    * DOC: also within CONTRIBUTING doc
    
    * BUG: linting fix for trailing whitespaces

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 The Python ARM Radar Toolkit, Py-ART, is an open source Python module containing
 a growing collection of weather radar algorithms and utilities build on top of
 the Scientific Python stack and distributed under the 3-Clause BSD license.
-Py-ART is used by the Atmospheric Radiation Measurement (ARM) Climate Research
+Py-ART is used by the Atmospheric Radiation Measurement (ARM) User
 Facility for working with data from a number of precipitation and cloud radars,
 but has been designed so that it can be used by others in the radar and
 atmospheric communities to examine, processes, and analyse data from many types
